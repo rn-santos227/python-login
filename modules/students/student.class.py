1107166b6d667cee7ad2698f02ccd08bd6efe543
rn-santos227 <rn_santos227@yahoo.com>
2024-04-28 00:58:18 +0800

    added level in student class

--- modules/students/student.class.py
+++ modules/students/student.class.py
@@ -1,9 +1,10 @@
 class Student:
   def __init__(self,
-    id, first_name, middle_name, last_name, section
+    id, first_name, middle_name, last_name, section, level
   ):
     self.id = id
     self.first_name = first_name
     self.middle_name = middle_name
     self.last_name = last_name
     self.section = section
+    self.level = level
