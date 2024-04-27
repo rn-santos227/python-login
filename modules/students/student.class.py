35d5bac2746824aed6e49304bb9395aae1729770
rn-santos227 <rn_santos227@yahoo.com>
2024-04-28 00:55:43 +0800

    added middle_name in student class

--- modules/students/student.class.py
+++ modules/students/student.class.py
@@ -1,6 +1,7 @@
 class Student:
   def __init__(self,
-    id, first_name,
+    id, first_name, middle_name
   ):
     self.id = id
     self.first_name = first_name
+    self.middle_name = middle_name
