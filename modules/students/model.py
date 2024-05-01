aa5a200fb5c95bad0c22373d51084f61e67414fe
rn-santos227 <rn_santos227@yahoo.com>
2024-05-01 22:19:11 +0800

    added student number in model

--- modules/students/model.py
+++ modules/students/model.py
@@ -1,9 +1,10 @@
 class Student:
   def __init__(self,
-    id, full_name, email, contact_number, section, level
+    id, full_name, student_number, email, contact_number, section, level
   ):
     self.id = id
     self.full_name = full_name
+    self.student_number = student_number
     self.email = email
     self.contact_number = contact_number
     self.section = section
