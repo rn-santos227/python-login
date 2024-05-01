70207cc7c27e83d884de2219b953ca1646fb891b
rn-santos227 <rn_santos227@yahoo.com>
2024-05-01 22:58:07 +0800

    added full name in admin model

--- modules/admin/model.py
+++ modules/admin/model.py
@@ -3,3 +3,4 @@ from modules.users.model import User
 class Admin(User):
   def __init__(self, id, full_name, email, password):
     super().__init__(id, email, password)
+    self.full_name = full_name
