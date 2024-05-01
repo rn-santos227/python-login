c71c3a8850e6a1551e97e59f4b01d4af174f1ac2
rn-santos227 <rn_santos227@yahoo.com>
2024-05-01 22:54:45 +0800

    update student model

--- modules/students/model.py
+++ modules/students/model.py
@@ -1,13 +1,10 @@
 from modules.users.model import User
 
-class Student():
-  def __init__(self,
-    id, full_name, student_number, email, contact_number, section, level
-  ):
-    self.id = id
+class Student(User):
+  def __init__(self, id, full_name, student_number, email, contact_number, section, level, password):
+    super().__init__(id, email, password)
     self.full_name = full_name
     self.student_number = student_number
-    self.email = email
     self.contact_number = contact_number
     self.section = section
     self.level = level
