9dda0b6d23466bcec187f5b3064311200da14e6d
rn-santos227 <rn_santos227@yahoo.com>
2024-05-10 22:33:49 +0800

    update validate email function

--- modules/users/model.py
+++ modules/users/model.py
@@ -17,6 +17,8 @@ class User:
   
   def validate_email(email):
     pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+    if re.match(pattern, email):
+      return email
 
   @property
   def password(self):
