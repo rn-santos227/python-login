aeb63ce7235b75704e622f1edc6cc54d6619a263
rn-santos227 <rn_santos227@yahoo.com>
2024-05-11 20:23:47 +0800

    added return student in get students by email function

--- modules/students/controller.py
+++ modules/students/controller.py
@@ -5,7 +5,7 @@ import config.database as DB
 def get_student_by_id(id):
   pass
 
-def get_student_by_email(email):
+def get_student_by_email(email) -> Student:
   sql_query = DB.query_builder('students', 'email = ?', 'select')
   cursor = DB.connect_db.cursor()
   cursor.execute(sql_query, (email))
@@ -13,7 +13,11 @@ def get_student_by_email(email):
 
   cursor.close()
   DB.connect_db.close()
-
+  if row:
+    student = Student(*row)
+    return student
+  else:
+    return "No student found with the provided email."
 
 def get_students():
   pass
