9730c297c7bbbcfe8e98e9f62285134c5df44123
rn-santos227 <rn_santos227@yahoo.com>
2024-05-13 21:26:50 +0800

    update get parents by id function in parents controller

--- modules/parents/controller.py
+++ modules/parents/controller.py
@@ -6,6 +6,7 @@ import config.database as DB
 def get_parent_by_id(id):
   sql_query = DB.query_builder('parents', 'id = ?', 'select')
   cursor = DB.connect_db.cursor()
+  cursor.execute(sql_query, (id))
 
 def get_parents(query, action) -> Union[list[Parent], str]:
   sql_query = DB.query_builder('parents', query, action)
