ccb5845b4088e004824bb8a4d8572c5190d11276
rn-santos227 <rn_santos227@yahoo.com>
2024-05-14 20:48:00 +0800

    update get admin by id function in admins controller

--- modules/admin/controller.py
+++ modules/admin/controller.py
@@ -7,6 +7,7 @@ from modules.admin.model import Admin
 def get_admin_by_id(id):
   sql_query = DB.query_builder('admins', 'id = ?', 'select')
   cursor = DB.connect_db.cursor()
+  cursor.execute(sql_query, (id))
 
 def get_admins(query, action) -> Union[list[Admin], str]:
   sql_query = DB.query_builder('admins', query, action)
