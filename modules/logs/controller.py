c7d38a886f0b185daab1be6abc2b527599b071aa
rn-santos227 <rn_santos227@yahoo.com>
2024-05-15 16:40:29 +0800

    update get log by id function in logs controller

--- modules/logs/controller.py
+++ modules/logs/controller.py
@@ -4,7 +4,7 @@ import config.database as DB
 from typing import Union
 from modules.logs.model import Log
 
-def get_log_by_id(id):
+def get_log_by_id(id) -> Union[Log, str]:
   sql_query = DB.query_builder('logs', 'id = ?', 'select')
   cursor = DB.connect_db.cursor()
   cursor.execute(sql_query, (id))
