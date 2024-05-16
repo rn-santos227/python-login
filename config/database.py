0388921d811f80806b9229738c283ff992d44b83
rn-santos227 <rn_santos227@yahoo.com>
2024-05-16 21:08:16 +0800

    update create database funciton

--- config/database.py
+++ config/database.py
@@ -5,7 +5,13 @@ import win32com.client
 from config.config import database_name
 
 def create_db():
-  pass
+  if not os.path.exists(database_name):
+    access_app = win32com.client.Dispatch("Access.Application")
+    access_app.NewCurrentDatabase(database_name)
+    access_app.Quit()
+    print(f"Database '{database_name}' created successfully.")
+  else:
+    print(f"Database '{database_name}' already exists.")
     
 def connect_db():
   conn_str = (
