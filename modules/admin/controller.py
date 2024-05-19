8f93d01c3814c6c1b6ed8a3d1feb90e32d3f23de
rn-santos227 <rn_santos227@yahoo.com>
2024-05-19 20:25:27 +0800

    update get admin by id in admin controller

--- modules/admin/controller.py
+++ modules/admin/controller.py
@@ -26,18 +26,22 @@ def get_admin_by_id(id) -> Union[Admin, str]:
 
 def get_admins(query, action) -> Union[list[Admin], str]:
   sql_query = DB.query_builder('admins', query, action)
-  cursor = DB.connect_db.cursor()
-  cursor.execute(sql_query)
-  rows = cursor.fetchall()
+  try:
+    cursor = DB.connect_db.cursor()
+    cursor.execute(sql_query)
+    rows = cursor.fetchall()
 
-  admins = []
-  for row in rows:
-    admin = Admin(*row)
-    admins.append(admin)
+    admins = []
+    for row in rows:
+      admin = Admin(*row)
+      admins.append(admin)
 
-  cursor.close()
-  DB.connect_db.close()
-  return admins if admins else "No admin records available."
+    cursor.close()
+    DB.connect_db.close()
+    return admins if admins else "No admin records available."
+  
+  except Exception as e:
+    print(f"Error: {e}")
 
 def create_admin(admin: Admin):
   pass
