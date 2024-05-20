1ea69a954f2358e37f5d5cd349425117cb044cc9
rn-santos227 <rn_santos227@yahoo.com>
2024-05-20 19:38:04 +0800

    update get logs by student function in logs controller

--- modules/logs/controller.py
+++ modules/logs/controller.py
@@ -59,12 +59,14 @@ def get_logs_by_student(student_id) -> Union[list[Log], str]:
       log = Log(*row)
       logs.append(log)
 
-    cursor.close()
-    DB.connect_db.close()
     return logs if logs else "No logs available."
   
   except Exception as e:
     print(f"Error: {e}")
+
+  finally:
+    cursor.close()
+    DB.connect_db.close()
 
 def create_log(log: Log) -> Log:
   pass
