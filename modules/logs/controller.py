8275930dc2016a1f14b8fefb3bbd738909b528e0
rn-santos227 <rn_santos227@yahoo.com>
2024-05-24 21:37:05 +0800

    update delete log function in logs controller

--- modules/logs/controller.py
+++ modules/logs/controller.py
@@ -142,3 +142,8 @@ def update_log(log: Log) -> Log:
 def delete_log(id):
   where_clause = f"id = {id}"
   sql_query = DB.query_builder(table, f"WHERE {where_clause}", 'delete')
+  try:
+    cursor = DB.connect_db.cursor()
+
+  except Exception as e:
+    print(f"Error: {e}")
