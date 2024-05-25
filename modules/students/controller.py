702bc7dbfb40d1e7bcf31381ac1025063c554d5e
rn-santos227 <rn_santos227@yahoo.com>
2024-05-25 22:17:58 +0800

    update delete student function in students controller

--- modules/students/controller.py
+++ modules/students/controller.py
@@ -112,6 +112,7 @@ def delete_student(id) -> bool:
   sql_query = DB.query_builder(table, f"WHERE {where_clause}", 'delete')
   try:
     cursor = DB.connect_db.cursor()
+    cursor.execute(sql_query)
 
   except Exception as e:
     print(f"Error: {e}")
