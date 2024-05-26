6bce399a9c3fe8800f816f70ee54e848c895953c
rn-santos227 <rn_santos227@yahoo.com>
2024-05-26 20:05:17 +0800

    update login function in auth controller

--- modules/auth/controller.py
+++ modules/auth/controller.py
@@ -2,4 +2,8 @@ from students.controller import get_student_by_email
 
 def login(email, password) -> bool:
   student = get_student_by_email(email)
-  return False
+  
+  if(student.verify_password(password)):
+    return True
+  else:
+    return False
