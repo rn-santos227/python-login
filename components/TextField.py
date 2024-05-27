4bed3984d27687288af18df4590ff2101320832e
rn-santos227 <rn_santos227@yahoo.com>
2024-05-27 22:49:50 +0800

    update text field components

--- components/TextField.py
+++ components/TextField.py
@@ -1,1 +1,5 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLineEdit, QLabel
+
+class DynamicTextField(QWidget):
+  def __init__(self, label_text="Dynamic Text Field", placeholder_text="Enter text...", parent=None):
+    super().__init__(parent)
