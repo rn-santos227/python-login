2d26548529d7654106a9efb6bb90c2b1011c26d2
rn-santos227 <rn_santos227@yahoo.com>
2024-05-28 00:19:20 +0800

    update init function in text field component

--- components/text_field.py
+++ components/text_field.py
@@ -1,8 +1,9 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLineEdit, QLabel
 
-class DynamicTextField(QWidget):
-  def __init__(self, label_text="Dynamic Text Field", placeholder_text="Enter text...", parent=None):
+class TextField(QWidget):
+  def __init__(self, label_text="Text Field", placeholder_text="Enter text...", parent=None):
     super().__init__(parent)
+    self.init_ui(label_text, placeholder_text)
 
   def init_ui(self, label_text, placeholder_text):
     pass
