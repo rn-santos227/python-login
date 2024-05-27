48c01bdf0a937375714e5641388ac05a0f5adb86
rn-santos227 <rn_santos227@yahoo.com>
2024-05-28 00:16:09 +0800

    added init ui function in text field component

--- components/TextField.py
+++ components/TextField.py
@@ -3,3 +3,6 @@ from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLineEdit, QLabel
 class DynamicTextField(QWidget):
   def __init__(self, label_text="Dynamic Text Field", placeholder_text="Enter text...", parent=None):
     super().__init__(parent)
+
+  def init_ui(self, label_text, placeholder_text):
+    pass
