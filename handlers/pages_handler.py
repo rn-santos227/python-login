e16a8230e00fccef45e1d0c7d209d9497204f668
rn-santos227 <rn_santos227@yahoo.com>
2024-05-29 20:54:23 +0800

    added switch to login page function in pages handler

--- handlers/pages_handler.py
+++ handlers/pages_handler.py
@@ -2,3 +2,6 @@ class PagesHandler:
   def __init__(self, stacked_widget, login_page):
     self.stacked_widget = stacked_widget
     self.login_page = login_page
+
+  def switch_to_login_page(self):
+    self.stacked_widget.setCurrentWidget(self.login_page)
