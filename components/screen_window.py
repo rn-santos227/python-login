afeb05af656edc79727b7ae04fbd2eb604909768
rn-santos227 <rn_santos227@yahoo.com>
2024-05-29 21:03:16 +0800

    updated init function in screen window component

--- components/screen_window.py
+++ components/screen_window.py
@@ -2,6 +2,8 @@ from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtWidgets import QApplication, QMainWindow, QStackedWidget
 from PyQt5.QtCore import Qt
 
+from handlers.pages_handler import PagesHandler
+
 from modules.auth.view import LoginPage
 
 class ScreenWindow(QMainWindow):
@@ -14,6 +16,10 @@ class ScreenWindow(QMainWindow):
     self.stacked_widget = QStackedWidget()
     self.setCentralWidget(self.stacked_widget)
 
+    self.page_handler = PagesHandler(self.stacked_widget, None)
+
     self.login_page = LoginPage(self)
-    
+
     self.stacked_widget.addWidget(self.login_page)
+
+
