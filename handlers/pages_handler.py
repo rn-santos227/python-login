b05ebaac09560949511a9fe749a88b30da4ff17b
rn-santos227 <rn_santos227@yahoo.com>
2024-05-29 20:53:25 +0800

    added login page in init function for pages handler

--- handlers/pages_handler.py
+++ handlers/pages_handler.py
@@ -1,3 +1,4 @@
 class PagesHandler:
-  def __init__(self, stacked_widget):
+  def __init__(self, stacked_widget, login_page):
     self.stacked_widget = stacked_widget
+    self.login_page = login_page
