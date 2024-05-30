63ed98440e12241c73f5ce7326481da072c3789c
rn-santos227 <rn_santos227@yahoo.com>
2024-05-30 22:18:49 +0800

    updated init ui function in auth view

--- modules/auth/view.py
+++ modules/auth/view.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QSpacerItem, QSizePolicy
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QSpacerItem, QSizePolicy
 from components.text_field import TextField
 
 from modules.auth.controller import login
@@ -12,26 +12,26 @@ class LoginPage(QWidget):
     self.init_ui()
 
   def init_ui(self):
-    layout = QVBoxLayout()
+    main_layout = QVBoxLayout()
+    center_layout = QVBoxLayout()
+    h_center_layout = QHBoxLayout()
 
     top_spacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
     bottom_spacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
 
     self.email_field = TextField(label_text="Email", placeholder_text="Enter your username")
     self.password_field = TextField(label_text="Password", placeholder_text="Enter your password")
-
     self.password_field.text_field.setEchoMode(QLineEdit.Password)
 
     login_button = QPushButton("Login")
     login_button.clicked.connect(self.handle_login)
 
-    layout.addItem(top_spacer)
-    layout.addWidget(QLabel("Login Page"))
-    layout.addWidget(self.email_field)
-    layout.addWidget(self.password_field)
-    layout.addItem(bottom_spacer)
+    center_layout.addItem(top_spacer)
+    center_layout.addWidget(QLabel("Login Page"))
+    center_layout.addWidget(self.email_field)
+    center_layout.addWidget(self.password_field)
+    center_layout.addItem(bottom_spacer)
 
-    self.setLayout(layout)
 
   def handle_login(self):
     email = self.email_field.get_text()
