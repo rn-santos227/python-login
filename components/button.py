6a1ff2b6d15570b13b2571285f7316238491c10f
rn-santos227 <rn_santos227@yahoo.com>
2024-06-02 02:19:08 +0800

    added init ui function in button component

--- components/button.py
+++ components/button.py
@@ -5,3 +5,12 @@ class Button(QWidget):
   def __init__(self, button_text="Button", parent=None):
     super().__init__(parent)
     self.init_ui(button_text)
+
+  def init_ui(self, button_text):
+    self.layout = QVBoxLayout()
+    self.button = QPushButton(button_text)
+    self.button.setFixedWidth(250) 
+
+    button_font = QFont()
+    button_font.setPointSize(12)
+    self.button.setFont(button_font)
