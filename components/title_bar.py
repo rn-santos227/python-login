b743e58fe9b3c4f946317a8a04c9a7ce0c72b64a
rn-santos227 <rn_santos227@yahoo.com>
2024-06-02 02:37:10 +0800

    added title bar class and init fucntion in title bar component

--- components/title_bar.py
+++ components/title_bar.py
@@ -1,2 +1,6 @@
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QPushButton, QLabel
 from PyQt5.QtCore import Qt
+
+class TitleBar(QWidget):
+  def __init__(self, parent=None):
+    super().__init__(parent)
