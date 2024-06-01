1d27491fcfcd03dae90c1b9846d5aafcdfdbb51e
rn-santos227 <rn_santos227@yahoo.com>
2024-06-02 02:43:05 +0800

    added init ui function in title bar component

--- components/title_bar.py
+++ components/title_bar.py
@@ -4,3 +4,7 @@ from PyQt5.QtCore import Qt
 class TitleBar(QWidget):
   def __init__(self, parent=None):
     super().__init__(parent)
+    self.init_ui()
+
+  def init_ui(self):
+    self.setFixedHeight(30) 
