7c2c216bbaa12418b3e556fd3926ef2a590c0372
rn-santos227 <rn_santos227@yahoo.com>
2024-06-02 02:17:50 +0800

    added button class and init function in button component

--- components/button.py
+++ components/button.py
@@ -1,2 +1,7 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPushButton
 from PyQt5.QtGui import QFont
+
+class Button(QWidget):
+  def __init__(self, button_text="Button", parent=None):
+    super().__init__(parent)
+    self.init_ui(button_text)
