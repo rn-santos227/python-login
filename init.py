04df7d640018838f6f841405a021ac853823ea6c
rn-santos227 <rn_santos227@yahoo.com>
2024-06-03 15:56:42 +0800

    updated initialized db function in init

--- init.py
+++ init.py
@@ -2,6 +2,7 @@ import config.database as db
 
 from modules.admin.model import Admin
 from modules.students.model import Student
+from modules.logs.model import Log
 
 def init():
   print("initializing app config.")
@@ -12,8 +13,9 @@ def init():
 def initialize_db():
   print("creating database...")
   db.create_db()
-  db.create_table(Admin.create_table, "admins")
-  db.create_table(Student.create_table, "students")
+  db.create_table(Admin.create_table(), "admins")
+  db.create_table(Student.create_table(), "students")
+  db.create_table(Log.create_table(), "logs")
 
 if __name__ == "__main__":
   init()
