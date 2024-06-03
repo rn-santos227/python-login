b8138d1bd0dc86ddcb4ce53d54e28fb29343b803
rn-santos227 <rn_santos227@yahoo.com>
2024-06-03 15:39:26 +0800

    updated init function

--- init.py
+++ init.py
@@ -2,6 +2,8 @@ import config.database as db
 
 def init():
   print("initializing app config.")
+  if(db.check_db_connection):
+    print("database has already been created.")
 
 if __name__ == "__main__":
   init()
