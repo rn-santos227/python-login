c7d2bfc59ddb99574bd0d67a8fe300726aa91254
rn-santos227 <rn_santos227@yahoo.com>
2024-06-04 17:27:35 +0800

    added alert message class in alert messaage component

--- components/alert_message.py
+++ components/alert_message.py
@@ -1,2 +1,6 @@
 from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout
 from PyQt5.QtCore import Qt
+
+class AlertMessage(QWidget):
+  def __init__(self, message="", alert_type="info", parent=None):
+    super().__init__(parent)
