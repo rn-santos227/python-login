f0978c3abc0aae4fb9dfda8807cea13ed3a72c23
rn-santos227 <rn_santos227@yahoo.com>
2024-06-04 12:28:22 +0800

    updated init function

--- init.py
+++ init.py
@@ -1,16 +1,11 @@
-import config.database as db
+import config.database as DB
 import modules.admin.controller as ctr_admin
 
 from modules.admin.model import Admin
-from modules.students.model import Student
-from modules.logs.model import Log
-from modules.parents.model import Parent
-
-
 
 def init():
   print("initializing app config.")
-  if(db.check_db_connection):
+  if(DB.check_db_connection):
     print("database has already been created.")
 
   else:
@@ -19,13 +14,10 @@ def init():
 
 def initialize_db():
   print("creating database...")
-  db.create_db()
+  DB.create_db()
 
   print("creating tables...")
-  db.create_table(Admin.create_table(), "admins")
-  db.create_table(Student.create_table(), "students")
-  db.create_table(Log.create_table(), "logs")
-  db.create_table(Parent.create_table(), "parents")
+
 
   print("creating default user...")
   admin = Admin(full_name="Administrator", email="test@test.com", password="test@123", status="active")
