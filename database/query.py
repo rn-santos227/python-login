67cedce4383b0f8ba8b5d8efedf0058dda6b2773
rn-santos227 <rn_santos227@yahoo.com>
2024-06-04 12:22:32 +0800

    updated builder function in database query

--- database/query.py
+++ database/query.py
@@ -1,7 +1,9 @@
 def builder(table, query, action):
+  sql_query = ""
   if action.lower() == 'select':
     pass
 
   else:
     raise ValueError("Invalid action. Supported actions are: 'select', 'insert', 'update', 'delete'.")
-  
+
+  return sql_query 
