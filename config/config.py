cba66909237543f103d241db36effe079a21cba8
rn-santos227 <rn_santos227@yahoo.com>
2024-06-06 21:10:07 +0800

    updated config file

--- config/config.py
+++ config/config.py
@@ -4,7 +4,7 @@ project_directory = os.path.dirname(os.path.abspath(__file__))
 
 files_folder = os.path.join(project_directory, 'data')
 
-database_name = "db_logbook.accdb"
+database_name = os.path.join(files_folder, "db_logbook.accdb")
 
 connection_string = (
   r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};"
