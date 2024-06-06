8c7ee75ea74409a0dc91745bb46d5d9b2a8c10b4
rn-santos227 <rn_santos227@yahoo.com>
2024-06-06 11:01:10 +0800

    updated init function in alert message component

--- components/alert_message.py
+++ components/alert_message.py
@@ -4,6 +4,7 @@ from PyQt5.QtCore import Qt
 class AlertMessage(QWidget):
   def __init__(self, message="", alert_type="info", parent=None):
     super().__init__(parent)
+    self.init_ui(message, alert_type)
 
   def init_ui(self, message, alert_type):
     pass
