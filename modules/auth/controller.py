6e276385c2ef3154a122e2059be9c2a015c38226
rn-santos227 <rn_santos227@yahoo.com>
2024-06-08 01:44:47 +0800

    imported admin and student models in auth controller

--- modules/auth/controller.py
+++ modules/auth/controller.py
@@ -1,5 +1,8 @@
-import modules.students.controller as student_controller
 import modules.admin.controller as admin_controller
+import modules.students.controller as student_controller
+
+from modules.admin.model import Admin
+from modules.students.model import Student
 
 def login(email, password) -> bool:
   student = student_controller.get_student_by_email(email)
