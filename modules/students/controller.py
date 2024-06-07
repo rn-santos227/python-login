288c0796d1c3487f8eebf3a20cdefc7e49068ad6
rn-santos227 <rn_santos227@yahoo.com>
2024-06-07 20:41:04 +0800

    updated get student by email in students controller

--- modules/students/controller.py
+++ modules/students/controller.py
@@ -45,7 +45,6 @@ def get_student_by_email(email) -> Union[Student, str]:
 
   finally:
     cursor.close()
-    DB.connect_db.close()
 
 def get_students(query, action) -> Union[list[Student], str]:
   sql_query = builder(table, query, action)
