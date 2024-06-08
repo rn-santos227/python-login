f2020d0a3cd1b1cf0ce3fcf689f19c94b27492cf
rn-santos227 <rn_santos227@yahoo.com>
2024-06-09 02:52:42 +0800

    updated init ui function in student view

--- modules/students/view.py
+++ modules/students/view.py
@@ -13,5 +13,27 @@ class StudentPage(QWidget):
     main_layout = QVBoxLayout()
     top_half_layout = QVBoxLayout()
 
-    create_layout = QHBoxLayout()
+    create_layout = QVBoxLayout()
+
+    self.email_field = TextField(label_text="Email", placeholder_text="Enter student email.")
+    self.password_field = TextField(label_text="Password", placeholder_text="Enter student password.")
+    self.password_field.text_field.setEchoMode(QLineEdit.Password)
+    self.fullname_field = TextField(label_text="Full Name", placeholder_text="Enter student full name.")
+    self.student_number_field = TextField(label_text="Student Number", placeholder_text="Enter student number.")
+    self.section_field = TextField(label_text="Student Section", placeholder_text="Enter student section.")
+    self.level_field = TextField(label_text="Student Level", placeholder_text="Enter student level.")
+
+
+    create_layout.addWidget(self.email_field)
+    create_layout.addWidget(self.password_field)
+    create_layout.addWidget(self.fullname_field)
+    create_layout.addWidget(self.student_number_field)
+    create_layout.addWidget(self.section_field)
+    create_layout.addWidget(self.level_field)
+
+    top_half_layout.addLayout(create_layout)
+
+    main_layout.addLayout(top_half_layout)
+  
+    self.setLayout(main_layout)
     
