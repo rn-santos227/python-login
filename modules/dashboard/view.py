ba2f1319404513de95977fe42304d2937b007bd2
rn-santos227 <rn_santos227@yahoo.com>
2024-06-09 01:07:12 +0800

    created init ui function in dashboard view

--- modules/dashboard/view.py
+++ modules/dashboard/view.py
@@ -1,6 +1,11 @@
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QStackedWidget
+from PyQt5.QtCore import Qt
 
 class DashboardPage(QWidget):
   def __init__(self, pages_handler):
     super().__init__()
     self.pages_handler = pages_handler
+    self.init_ui()
+
+  def init_ui(self):
+    layout = QHBoxLayout(self)
