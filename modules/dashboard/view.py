cbe28d8b3d8e4dcec4cba2b4aef1b7139796126d
rn-santos227 <rn_santos227@yahoo.com>
2024-06-09 01:10:28 +0800

    updated init ui function dashboard view

--- modules/dashboard/view.py
+++ modules/dashboard/view.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QStackedWidget
 from PyQt5.QtCore import Qt
 
+from components.button import Button
+
 class DashboardPage(QWidget):
   def __init__(self, pages_handler):
     super().__init__()
@@ -9,3 +11,20 @@ class DashboardPage(QWidget):
 
   def init_ui(self):
     layout = QHBoxLayout(self)
+
+    self.navigation_menu = QVBoxLayout()
+    self.main_content = QStackedWidget()
+
+    dashboard_button = Button("Dashboard")
+
+    students_button = Button("Students")
+
+    logs_button = Button("Attendance Logs")
+
+    parents_button = Button("Parents")
+
+    self.navigation_menu.addWidget(dashboard_button)
+    self.navigation_menu.addWidget(students_button)
+    self.navigation_menu.addWidget(logs_button)
+    self.navigation_menu.addWidget(parents_button)
+    self.navigation_menu.addStretch()
