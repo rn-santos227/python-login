74782020f2975ebbd16ab566b33d3b95c7624fd5
rn-santos227 <rn_santos227@yahoo.com>
2024-06-10 01:24:12 +0800

    updated message box in components

--- components/message_box.py
+++ components/message_box.py
@@ -10,10 +10,13 @@ class MessageBox:
     msg_box.setText(message)
 
     if message_type == "information":
-        msg_box.setIcon(QMessageBox.Information)
+      msg_box.setIcon(QMessageBox.Information)
     elif message_type == "warning":
-        msg_box.setIcon(QMessageBox.Warning)
+      msg_box.setIcon(QMessageBox.Warning)
     elif message_type == "error":
-        msg_box.setIcon(QMessageBox.Critical)
+      msg_box.setIcon(QMessageBox.Critical)
     elif message_type == "question":
-        msg_box.setIcon(QMessageBox.Question)
+      msg_box.setIcon(QMessageBox.Question)
+
+    msg_box.setStandardButtons(QMessageBox.Ok)
+    msg_box.exec_()
