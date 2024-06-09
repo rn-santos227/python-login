5d4f002fe8d9d0fc9abaa5d5bbff33db58cbdd62
rn-santos227 <rn_santos227@yahoo.com>
2024-06-10 01:23:06 +0800

    updated message box in components

--- components/message_box.py
+++ components/message_box.py
@@ -3,3 +3,6 @@ from PyQt5.QtWidgets import QMessageBox
 class MessageBox:
   def __init__(self, parent=None):
     self.parent = parent
+
+  def show_message(self, title, message, message_type="information"):
+    msg_box = QMessageBox(self.parent)
