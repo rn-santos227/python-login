59841d81770c4f145ac46f25d0ef099608d2834e
rn-santos227 <rn_santos227@yahoo.com>
2024-06-11 00:50:23 +0800

    created init ui function in admin view

--- modules/admin/view.py
+++ modules/admin/view.py
@@ -13,3 +13,34 @@ class AdminsPage(QWidget):
   def __init__(self, pages_handler):
     super().__init__()
     self.pages_handler = pages_handler
+    self.init_ui()
+
+  def init_ui(self):
+    main_layout = QHBoxLayout()
+    left_layout = QHBoxLayout()
+    button_layout = QHBoxLayout()
+
+    create_layout = QVBoxLayout()
+
+    self.email_field = TextField(label_text="Email", placeholder_text="Enter admin email.")
+    self.password_field = TextField(label_text="Password", placeholder_text="Enter admin password.")
+    self.password_field.text_field.setEchoMode(QLineEdit.Password)
+    self.fullname_field = TextField(label_text="Full Name", placeholder_text="Enter admin full name.")
+
+    create_button = Button("Create Admin")
+    create_button.connect_signal(self.create_student)
+
+    button_layout.addSpacerItem(QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum))
+    button_layout.addWidget(create_button)
+
+    create_layout.addWidget(self.email_field)
+    create_layout.addWidget(self.password_field)
+    create_layout.addWidget(self.fullname_field)
+    create_layout.addLayout(button_layout)
+
+    left_layout.addLayout(create_layout)
+
+    self.table_widget = QTableWidget()
+    self.table_widget.setColumnCount(4)
+    self.table_widget.setHorizontalHeaderLabels(["ID", "Full Name", "Email", "Actions"])
+    self.table_widget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
