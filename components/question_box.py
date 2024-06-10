780717ea5284635a46c97e034b298ab6c43a5f44
rn-santos227 <rn_santos227@yahoo.com>
2024-06-11 00:11:58 +0800

    added init ui function in question box

--- components/question_box.py
+++ components/question_box.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLabel, QHBoxLayout, QPushButton
 
-class DynamicQuestionBox(QDialog):
+class QuestionBox(QDialog):
   def __init__(self, title="Question", message="Are you sure?", parent=None):
     super().__init__(parent)
     self.setWindowTitle(title)
@@ -12,3 +12,13 @@ class DynamicQuestionBox(QDialog):
     self.layout.addWidget(self.message_label)
 
     self.button_layout = QHBoxLayout()
+    self.yes_button = QPushButton("Yes")
+    self.yes_button.clicked.connect(self.accept)
+    self.button_layout.addWidget(self.yes_button)
+    
+    self.no_button = QPushButton("No")
+    self.no_button.clicked.connect(self.reject)
+    self.button_layout.addWidget(self.no_button)
+    
+    self.layout.addLayout(self.button_layout)
+    self.setLayout(self.layout)
