064094737cbc969203876df720543506c996df9b
rn-santos227 <rn_santos227@yahoo.com>
2024-06-11 00:32:46 +0800

    updated init function in parents view

--- modules/parents/view.py
+++ modules/parents/view.py
@@ -1,7 +1,17 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QStackedWidget
 from PyQt5.QtCore import Qt
 
+from components.button import Button
+from components.message_box import MessageBox
+from components.text_field import TextField
+
+from handlers.validations_handler import ValidationHandler
+from modules.parents.model import Parent
+
 class ParentsPage(QWidget):
   def __init__(self, pages_handler):
     super().__init__()
     self.pages_handler = pages_handler
+    self.message_box = MessageBox(self)
+    self.validation_handler = ValidationHandler()
+    self.parents = []
