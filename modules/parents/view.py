e06e2accb21a8dde7093af25a1b52cfc5b7ee925
rn-santos227 <rn_santos227@yahoo.com>
2024-06-16 19:42:40 +0800

    updated init ui function in parents view

--- modules/parents/view.py
+++ modules/parents/view.py
@@ -20,37 +20,20 @@ class ParentsPage(QWidget):
     self.init_ui()
 
   def init_ui(self):
-    main_layout = QVBoxLayout()
-    top_layout = QHBoxLayout()
-    button_layout = QHBoxLayout()
-  
-    create_layout = QGridLayout()
+    self.main_layout = QVBoxLayout()
+    self.top_layout = QHBoxLayout()
 
-    self.student_name_field = TextField(label_text="Student Name", placeholder_text="Enter student name.")
-    self.parent_name_field = TextField(label_text="Parent Full Name", placeholder_text="Enter parent full name.")
-    self.parent_contact_field = TextField(label_text="Contact Number", placeholder_text="Enter parent contact number.")
-
-    create_button = Button("Create Parent")
-
-    button_layout.addSpacerItem(QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum))
-    button_layout.addWidget(create_button)
-
-    create_layout.addWidget(self.student_name_field, 0, 0, 1, 2)
-    create_layout.addWidget(self.parent_name_field, 1, 0, 1, 2)
-    create_layout.addWidget(self.parent_contact_field, 2, 0, 1, 2)
-    create_layout.addLayout(button_layout, 3, 0, 1, 2)
-
-    top_layout.addLayout(create_layout)
+    self.top_layout.addLayout(self.init_create_layout())
 
     self.table_widget = QTableWidget()
     self.table_widget.setColumnCount(5)
     self.table_widget.setHorizontalHeaderLabels(["ID", "Student Name", "Parent Name", "Contact Number", "Actions"])
     self.table_widget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
-    main_layout.addLayout(top_layout)
-    main_layout.addWidget(self.table_widget)
+    self.main_layout.addLayout(self.top_layout)
+    self.main_layout.addWidget(self.table_widget)
   
-    self.setLayout(main_layout)
+    self.setLayout(self.main_layout)
 
   def init_create_layout(self):
     create_layout = QGridLayout()
