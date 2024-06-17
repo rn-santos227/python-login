d145fb87cfd4551d76ca20830ffefe3a5364c902
rn-santos227 <rn_santos227@yahoo.com>
2024-06-17 18:25:36 +0800

    added combo box class in combo box component

--- components/combo_box.py
+++ components/combo_box.py
@@ -1,2 +1,6 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QComboBox
 from PyQt5.QtGui import QFont
+
+class ComboBox(QWidget):
+  def __init__(self, label_text="Combo Box", items=None, parent=None):
+    super().__init__(parent)
