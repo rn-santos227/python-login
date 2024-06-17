ddbaa7c4a0eac0082c656a17b5f17e8995e248c6
rn-santos227 <rn_santos227@yahoo.com>
2024-06-17 19:32:31 +0800

    updated init function in combo box component

--- components/combo_box.py
+++ components/combo_box.py
@@ -4,6 +4,7 @@ from PyQt5.QtGui import QFont
 class ComboBox(QWidget):
   def __init__(self, label_text="Combo Box", items=None, parent=None):
     super().__init__(parent)
+    self.init_ui(label_text, items)
 
   def init_ui(self, label_text, items):
     self.layout = QVBoxLayout()
