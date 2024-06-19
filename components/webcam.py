a39bf91e6114056aae375fe1666d2385e63c9d16
rn-santos227 <rn_santos227@yahoo.com>
2024-06-20 00:19:30 +0800

    added init ui funcion in webcam component

--- components/webcam.py
+++ components/webcam.py
@@ -1,6 +1,12 @@
 from PyQt5 import QtCore, QtGui
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel
 
 class WebcamComponent(QWidget):
   def __init__(self, parent=None):
     super().__init__(parent)
+
+  def init_ui(self):
+    self.layout = QVBoxLayout(self)
+
+    self.label = QLabel(self)
+    self.layout.addWidget(self.label)
