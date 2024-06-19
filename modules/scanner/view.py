b34f38bc77854ed165e7310c4f6650266dae7f0a
rn-santos227 <rn_santos227@yahoo.com>
2024-06-19 21:57:35 +0800

    updated init ui function in scanner view

--- modules/scanner/view.py
+++ modules/scanner/view.py
@@ -2,8 +2,10 @@
 from PyQt5.QtWidgets import QWidget
 
 class ScannerPage(QWidget):
-  def __init__(self):
+  def __init__(self, pages_handler):
     super().__init__()
+    self.pages_handler = pages_handler
+    self.students = []
 
   def init_ui(self):
     pass
