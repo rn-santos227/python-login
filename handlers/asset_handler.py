e75ecfa27ea63fcaf96a3d963b5d8cdebfb40a34
rn-santos227 <rn_santos227@yahoo.com>
2024-06-20 20:46:49 +0800

    added get image function in assets handler

--- handlers/asset_handler.py
+++ handlers/asset_handler.py
@@ -4,3 +4,6 @@ from PyQt5.QtGui import QPixmap
 class AssetHandler:
   def __init__(self, assets_dir="assets"):
     self.assets_dir = assets_dir
+
+  def get_image(self, image_name):
+    image_path = os.path.join(self.assets_dir, image_name)
