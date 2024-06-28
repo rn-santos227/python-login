6cdb9538eedfb67ffc7479dbacf3bc1802ead3b9
rn-santos227 <rn_santos227@yahoo.com>
2024-06-28 21:34:31 +0800

    updated init ui function in scanner view

--- modules/scanner/view.py
+++ modules/scanner/view.py
@@ -31,7 +31,10 @@ class ScannerPage(QWidget):
 
     self.student_combo_box = ComboBox(label_text="Student Names", items=self.load_students_to_combo_box())
     self.webcam_component = Webcam(self)
+
+    webcam_center_layout.addItem(left_spacer)
     webcam_center_layout.addWidget(self.webcam_component)
+    webcam_center_layout.addItem(right_spacer)
 
     self.start_button = Button("Start Webcam")
     self.start_button.connect_signal(self.__enable_capture)
