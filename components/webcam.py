866e78900de5c6ff8239827002f275812c99c40e
rn-santos227 <rn_santos227@yahoo.com>
2024-06-29 21:42:06 +0800

    updated detect face function in webcam component

--- components/webcam.py
+++ components/webcam.py
@@ -41,6 +41,8 @@ class Webcam(QWidget):
     for (x, y, w, h) in faces:
       cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
+    return img
+
   def capture_image(self):
     ret, frame = self.cap.read()
     return ret, frame
