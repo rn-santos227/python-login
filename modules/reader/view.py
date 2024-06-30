8c2c0c7434d3f2066dd9c8734e4821b5d585007c
rn-santos227 <rn_santos227@yahoo.com>
2024-06-30 14:23:02 +0800

    added init ui function in reader view

--- modules/reader/view.py
+++ modules/reader/view.py
@@ -10,3 +10,9 @@ class ReaderPage(QWidget):
   def __init__(self, pages_handler):
     super().__init__()
     self.pages_handler = pages_handler
+
+  def init_ui(self):
+    self.main_layout = QVBoxLayout()
+    center_layout = QVBoxLayout()
+    h_center_layout = QHBoxLayout()
+    webcam_center_layout = QHBoxLayout()
