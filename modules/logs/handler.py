47526b36acb503768155e355502af7dd387486f2
rn-santos227 <rn_santos227@yahoo.com>
2024-07-07 01:30:49 +0800

    added update logs function in logs handler

--- modules/logs/handler.py
+++ modules/logs/handler.py
@@ -4,3 +4,7 @@ from modules.logs.model import Log
 from modules.logs.controller import get_logs
 
 logs: List[Log] = get_logs("status = 'active'", "select")
+
+def update_logs():
+  global logs
+  logs = get_logs("status = 'active'", "select")
