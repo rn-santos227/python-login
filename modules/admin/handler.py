3eeb86f1165b2d1f13fded749fd0030154d1cbd4
rn-santos227 <rn_santos227@yahoo.com>
2024-07-07 01:12:59 +0800

    added update admins function in admins handler

--- modules/admin/handler.py
+++ modules/admin/handler.py
@@ -4,3 +4,7 @@ from modules.admin.model import Admin
 from modules.admin.controller import get_admins
 
 admins: List[Admin] = get_admins("status = 'active", "select")
+
+def update_admins():
+  global admins
+  admins = get_admins("status = 'active'", "select")
