4c8a7ac74558069e9f3d9eb6cf5f73e42bff2298
rn-santos227 <rn_santos227@yahoo.com>
2024-07-07 00:40:01 +0800

    added udpate students function in students handler

--- modules/students/handler.py
+++ modules/students/handler.py
@@ -4,3 +4,7 @@ from modules.students.model import Student
 from modules.students.controller import get_students
 
 students: List[Student] = get_students("status = 'active'", "select")
+
+def update_students():
+  global students
+  students = get_students("status = 'active'", "select")
