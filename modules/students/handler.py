9fe0af6efc8c0c3451fb3e58ecaf2963b2b458e6
rn-santos227 <rn_santos227@yahoo.com>
2024-07-06 21:53:04 +0800

    updated students handler

--- modules/students/handler.py
+++ modules/students/handler.py
@@ -1,4 +1,6 @@
 from typing import List
-from modules.students.model import Student
 
-students: List[Student] = []
+from modules.students.model import Student
+from modules.students.controller import get_students
+
+students: List[Student] = get_students("status = 'active'", "select")
