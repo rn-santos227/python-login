43638881b78373ac7d72348a7d8eb703dce77ed6
rn-santos227 <rn_santos227@yahoo.com>
2024-07-06 21:59:26 +0800

    updated view in logs module

--- modules/logs/view.py
+++ modules/logs/view.py
@@ -2,19 +2,20 @@ import modules.logs.controller as logs_controller
 
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QLabel, QLineEdit, QPushButton, QTableWidget, QTableWidgetItem, QHeaderView, QMessageBox,  QSpacerItem, QSizePolicy
 
+from modules.logs.handler import logs
+
 class LogsPage(QWidget):
   def __init__(self, pages_handler):
     super().__init__()
     self.pages_handler = pages_handler
-    self.logs = []
     self.init_ui()
 
   def init_ui(self):
     main_layout = QHBoxLayout()
 
     self.table_widget = QTableWidget()
-    self.table_widget.setColumnCount(4)
-    self.table_widget.setHorizontalHeaderLabels(["Student Name", "Time Login", "Time Logout", "Actions"])
+    self.table_widget.setColumnCount(5)
+    self.table_widget.setHorizontalHeaderLabels(["Student Name", "Date", "Time Login", "Time Logout", "Actions"])
     self.table_widget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
     main_layout.addWidget(self.table_widget)
