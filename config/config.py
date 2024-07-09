3608dc1755b5643e3f9e8b867977a7613e46ce54
rn-santos227 <rn_santos227@yahoo.com>
2024-07-09 15:08:44 +0800

    updated configuration file connection params

--- config/config.py
+++ config/config.py
@@ -1,10 +1,6 @@
-import os
-
-documents_directory = os.path.join(os.path.expanduser('~'), 'Documents')
-
-database_path = os.path.join(documents_directory, "db_logbook.mdb")
-
-connection_string = (
-  r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};"
-  r"DBQ=" + database_path + ";"
-)
+connection_params = {
+  "user": "root",
+  "password": "",
+  "host": "localhost",
+  "raise_on_warnings": True,
+}
