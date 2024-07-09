f7b9afe7f0a1ba5e49e14210f8ceeebd43bf95cc
rn-santos227 <rn_santos227@yahoo.com>
2024-07-09 16:07:27 +0800

    updated create db function in database config

--- config/database.py
+++ config/database.py
@@ -7,6 +7,7 @@ from config.config import database_name, connection_params
 def create_db():
   try:
     conn = mysql.connector.connect(**connection_params)
+    cursor = conn.cursor()
 
   except mysql.connector.Error as err:
     pass
