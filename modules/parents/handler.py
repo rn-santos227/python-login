024ac259bdcf3c6111b92b47371170ebd4f8c505
rn-santos227 <rn_santos227@yahoo.com>
2024-07-10 17:28:23 +0800

    added update parents function in  parents handler

--- modules/parents/handler.py
+++ modules/parents/handler.py
@@ -4,3 +4,7 @@ from modules.parents.model import Parent
 from modules.parents.controller import get_parents
 
 parents: List[Parent] = get_parents("all", "select")
+
+def update_parents():
+  global parents
+  parents = get_parents("all", "select")
