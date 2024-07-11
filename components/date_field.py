f9688a7f99dbb8ad67b2ece62a55ca576391550e
rn-santos227 <rn_santos227@yahoo.com>
2024-07-11 19:35:02 +0800

    updated init ui function in date field component

--- components/date_field.py
+++ components/date_field.py
@@ -5,5 +5,8 @@ class DateField(QWidget):
   def __init__(self, label_text="Select Date", parent=None):
     super().__init__(parent)
 
-  def init_ui(self):
+  def init_ui(self, label_text):
     self.layout = QVBoxLayout()
+
+    self.label = QLabel(label_text)
+    self.date_edit = QDateEdit()
