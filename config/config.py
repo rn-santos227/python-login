fedae74d1d096f9299f69ef2816fe5055e74b887
rn-santos227 <rn_santos227@yahoo.com>
2024-07-11 19:06:02 +0800

    updated config

--- config/config.py
+++ config/config.py
@@ -7,7 +7,7 @@ connection_params = {
   "user": os.getenv("DB_USER"),
   "password": os.getenv("DB_PASSWORD"),
   "host": os.getenv("DB_HOST"),
-  "raise_on_warnings": True,
+  "raise_on_warnings": os.getenv("DB_RAISE_ON_WARNINGS") == "True",
 }
 
 database_name = os.getenv("DB_NAME")
