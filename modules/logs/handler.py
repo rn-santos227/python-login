3935491924072bc42a8deb10e7af74476f529267
rn-santos227 <rn_santos227@yahoo.com>
2024-07-12 22:23:29 +0800

    updated udpate logs function in logs handler

--- modules/logs/handler.py
+++ modules/logs/handler.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 from datetime import datetime
 
 from modules.logs.model import Log
@@ -6,8 +6,8 @@ from modules.logs.controller import get_logs
 
 logs: List[Log] = get_logs("all", "select")
 
-def update_logs(start_date, end_date):
+def update_logs(start_date: Optional[str] = None, end_date: Optional[str] = None):
   global logs
   current_date = datetime.now().strftime("%Y-%m-%d")
-  
+
   logs = get_logs("all", "select")
