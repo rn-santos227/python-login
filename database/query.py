9b930817787abd312edb0788854dae00f6b98cdc
rn-santos227 <rn_santos227@yahoo.com>
2024-07-13 16:19:59 +0800

    updated join builder function in database query

--- database/query.py
+++ database/query.py
@@ -33,3 +33,6 @@ def join_builder(table1, table2, join_condition=None, join_type=None, query=None
   if query.lower() == 'all':
     if table2 and join_condition:
       sql_query = f"SELECT * FROM {table1} {join_type} JOIN {table2} ON {join_condition};"
+      
+    else:
+      sql_query = f"SELECT * FROM {table1};"
