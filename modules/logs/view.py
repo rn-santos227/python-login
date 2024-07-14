3171cf8d79279ff8b753684a04031e7e75682a1d
rn-santos227 <rn_santos227@yahoo.com>
2024-07-14 13:45:30 +0800

    updated load logs function in logs view

--- modules/logs/view.py
+++ modules/logs/view.py
@@ -74,6 +74,9 @@ class LogsPage(QWidget):
       button_layout.addWidget(delete_button)
       button_layout.setContentsMargins(0, 0, 0, 0)
 
+      button_widget = QWidget()
+      button_widget.setLayout(button_layout)
+
   def delete_log(self, log_id):
     pass
 
