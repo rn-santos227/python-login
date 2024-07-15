a948b8173dfd27d8c9716cfb21c5a8c52a2e3f18
rn-santos227 <rn_santos227@yahoo.com>
2024-07-15 19:26:55 +0800

    updates sms handler

--- handlers/sms_handler.py
+++ handlers/sms_handler.py
@@ -2,7 +2,7 @@ import requests
 
 from config.config import app_name, sms_api
 
-def send_sms(student, contact_number):
+def send_sms(contact_number, message):
   data = {
     "apikey": sms_api,
     "sendername": app_name,
