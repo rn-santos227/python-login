2ab7e1a2d70f8c8ee2559a7571bea63822bae174
rn-santos227 <rn_santos227@yahoo.com>
2024-07-16 16:21:17 +0800

    updated send sms to parents function in reader view

--- modules/reader/view.py
+++ modules/reader/view.py
@@ -136,7 +136,7 @@ class ReaderPage(QWidget):
     self.webcam_button.connect_signal(self.__enable_capture)
 
 
-  def __send_sms_to_parents(student: Student, logged: str):
+  def __send_sms_to_parents(student: Student, time: str, logged: str):
     parents = get_parents(f"student_id = {student.id}", "select")
     for parent in parents:
       pass
