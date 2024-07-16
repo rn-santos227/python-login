4056acf265e8a7bb0bd423b46437907a4e1ed0e0
rn-santos227 <rn_santos227@yahoo.com>
2024-07-16 16:24:17 +0800

    updated match face function in reader view

--- modules/reader/view.py
+++ modules/reader/view.py
@@ -116,6 +116,7 @@ class ReaderPage(QWidget):
           self.message_box.show_message("Information", f"Student: {student.full_name} has logged out on {formatted_date_time}", "information")
           logout_message = compose_message(student=student, time=formatted_date_time, logged="logged out")
           send_sms(contact_number=student.contact_number, message=logout_message)
+          self.__send_sms_to_parents(student=student, time=formatted_date_time, logged="logged out")
 
         update_logs()
         return
