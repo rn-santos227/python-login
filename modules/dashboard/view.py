ea26de41e8d389fc5f2a1afd83f825d4c382e76b
rn-santos227 <rn_santos227@yahoo.com>
2024-07-17 20:59:28 +0800

    updated dashboard view

--- modules/dashboard/view.py
+++ modules/dashboard/view.py
@@ -1,5 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QStackedWidget, QDialog
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QDialog, QHBoxLayout, QStackedWidget, QVBoxLayout, QWidget
 
 from components.button import Button
 from components.question_box import QuestionBox
