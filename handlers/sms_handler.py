d3ea67e44223d754971774478712db7c856cd3df
rn-santos227 <rn_santos227@yahoo.com>
2024-07-19 23:03:32 +0800

    updated send sms function in sms handler

--- handlers/sms_handler.py
+++ handlers/sms_handler.py
@@ -7,7 +7,6 @@ from modules.students.model import Student
 def send_sms(contact_number, message):
   data = {
     "apikey": sms_key,
-    "sendername": app_name,
     "number": contact_number,
     "message": message,
   }
