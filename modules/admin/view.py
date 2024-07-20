5621e30a719c6b7d85b776926e989fd12203b36d
rn-santos227 <rn_santos227@yahoo.com>
2024-07-20 13:27:43 +0800

    updated tables in logs view

--- modules/admin/view.py
+++ modules/admin/view.py
@@ -30,6 +30,7 @@ class AdminsPage(QWidget):
     self.table_widget.setColumnCount(4)
     self.table_widget.setHorizontalHeaderLabels(["ID", "Full Name", "Email", "Actions"])
     self.table_widget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+    self.table_widget.verticalHeader().setVisible(False)
 
     self.main_layout.addLayout(self.top_layout)
     self.main_layout.addWidget(self.table_widget)
