cacca9c1b29d36a3715eb5c0d215e093827c2d6f
rn-santos227 <rn_santos227@yahoo.com>
2024-07-20 13:40:24 +0800

    updated match face function in reader view

--- modules/reader/view.py
+++ modules/reader/view.py
@@ -113,7 +113,7 @@ class ReaderPage(QWidget):
           self.message_box.show_message("Information", f"Student: {student.full_name} has logged in on {formatted_date_time}", "information")
           login_message = compose_message(student=student, time=formatted_date_time, logged="logged in")
           send_sms(contact_number=student.contact_number, message=login_message)
-          self.__send_sms_to_parents(student, time=formatted_date_time, logged="logged in")
+          self.__send_sms_to_parents(student, formatted_date_time, "logged in")
 
         else:
           if log.logout_time is not None:
