09b9d13b990c5e7d6709396d3fee959b0fb816d3
rn-santos227 <rn_santos227@yahoo.com>
2024-07-21 14:19:10 +0800

    updated clock class in clock components

--- components/clock.py
+++ components/clock.py
@@ -1,10 +1,10 @@
 import sys
 import math
 
-from PyQt5.QtCore import Qt, QTimer, QTime
-from PyQt5.QtGui import QPainter, QColor, QPen
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtWidgets import QLabel, QVBoxLayout, QWidget
 
 class Clock(QWidget):
   def __init__(self):
     super().__init__()
+    self.timer = QtCore.QTimer(self)
