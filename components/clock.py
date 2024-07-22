3cfc38ea5f60053892f18d64c92cc3f4e291ed88
rn-santos227 <rn_santos227@yahoo.com>
2024-07-22 12:15:39 +0800

    update display clock function in clock component

--- components/clock.py
+++ components/clock.py
@@ -42,3 +42,4 @@ class Clock(QWidget):
     painter.save()
     painter.rotate(30.0 * (time.hour() + time.minute() / 60.0))
     painter.setPen(QtGui.QPen(QtCore.Qt.black, 6, QtCore.Qt.SolidLine, QtCore.Qt.RoundCap, QtCore.Qt.RoundJoin))
+    painter.drawLine(0, 0, 50, 0)
