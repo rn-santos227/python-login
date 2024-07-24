5566f69b65e9f07778739ca1271443670db368d9
rn-santos227 <rn_santos227@yahoo.com>
2024-07-24 12:35:58 +0800

    updated init ui function in auth view

--- modules/auth/view.py
+++ modules/auth/view.py
@@ -34,7 +34,7 @@ class LoginPage(QWidget):
     form_frame.setFrameShape(QFrame.Box)
     form_frame.setFrameShadow(QFrame.Raised)
     form_frame.setStyleSheet("background-color: rgba(255, 255, 255, 220);")
-    form_layout = QVBoxLayout(form_frame)
+    form_layout: QVBoxLayout = QVBoxLayout(form_frame)
 
     top_spacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
     bottom_spacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
