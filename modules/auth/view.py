9531fbe61ac375ec17a8a55d1093bb227aeec807
rn-santos227 <rn_santos227@yahoo.com>
2024-07-24 12:35:30 +0800

    updated init ui function in auth view

--- modules/auth/view.py
+++ modules/auth/view.py
@@ -27,7 +27,7 @@ class LoginPage(QWidget):
     self.background_label.lower()
 
     center_layout: QVBoxLayout = QVBoxLayout()
-    h_center_layout = QHBoxLayout()
+    h_center_layout: QHBoxLayout = QHBoxLayout()
     button_layout = QHBoxLayout()
 
     form_frame = QFrame(self)
