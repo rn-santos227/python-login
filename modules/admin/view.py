e242ff9c1cf7b41eee200d34d56c663934bedace
rn-santos227 <rn_santos227@yahoo.com>
2024-07-25 08:43:05 +0800

    updated init update function in admin view

--- modules/admin/view.py
+++ modules/admin/view.py
@@ -65,7 +65,7 @@ class AdminsPage(QWidget):
     self.update_button_layout = QHBoxLayout()
 
     self.update_email_field: TextField = TextField(label_text="Email", placeholder_text="Enter admin email.")
-    self.update_password_field = TextField(label_text="Password", placeholder_text="Enter admin password.")
+    self.update_password_field: TextField = TextField(label_text="Password", placeholder_text="Enter admin password.")
     self.update_password_field.text_field.setEchoMode(QLineEdit.Password)
     self.update_fullname_field = TextField(label_text="Full Name", placeholder_text="Enter admin full name.")
 
