b08540a6b17438016f805aa734ce65831fb57eac
rn-santos227 <rn_santos227@yahoo.com>
2024-07-25 08:47:42 +0800

    updated init ui function in dashboard view

--- modules/dashboard/view.py
+++ modules/dashboard/view.py
@@ -21,7 +21,7 @@ class DashboardAdminPage(QWidget):
     self.admins_content: AdminsPage = AdminsPage(self)
     self.logs_content: LogsPage = LogsPage(self)
     self.parents_content: ParentsPage = ParentsPage(self)
-    self.reader_content = ReaderPage(self)
+    self.reader_content: ReaderPage = ReaderPage(self)
     self.scanner_content = ScannerPage(self)
     self.students_content = StudentPage(self)
 
