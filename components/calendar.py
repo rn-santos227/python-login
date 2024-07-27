fe9784329f80d46d5b4700d393f5a37a30c908ba
rn-santos227 <rn_santos227@yahoo.com>
2024-07-27 18:19:09 +0800

    added init ui function in calendar component

--- components/calendar.py
+++ components/calendar.py
@@ -1,6 +1,9 @@
 from PyQt5.QtWidgets import QLabel, QVBoxLayout, QWidget
 
-
 class Calendar(QWidget):
   def __init__(self):
-     super().__init__()
+    super().__init__()
+    self.init_ui()
+
+  def init_ui(self):
+    self.layout: QVBoxLayout = QVBoxLayout(self)
