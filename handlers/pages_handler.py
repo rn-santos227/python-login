e38aa19d4d1f83f2e40e150e2c4e2e2040c43ce2
rn-santos227 <rn_santos227@yahoo.com>
2024-07-31 19:06:36 +0800

    updated init function in pages handler

--- handlers/pages_handler.py
+++ handlers/pages_handler.py
@@ -5,7 +5,7 @@ from modules.dashboard.view import DashboardAdminPage
 class PagesHandler:
   def __init__(self, stacked_widget):
     self.stacked_widget = stacked_widget
-    self.login_page = LoginPage(self)
+    self.login_page: LoginPage = LoginPage(self)
     self.dashboard_page = DashboardAdminPage(self)
 
   def switch_to_login_page(self):
