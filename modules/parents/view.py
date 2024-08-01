bac5941aa5364ac57e9d21f0321b85aeedd0dc2a
rn-santos227 <rn_santos227@yahoo.com>
2024-08-01 08:20:09 +0800

    updated load parents function in parents view

--- modules/parents/view.py
+++ modules/parents/view.py
@@ -130,7 +130,7 @@ class ParentsPage(QWidget):
       button_layout.addWidget(delete_button)
       button_layout.setContentsMargins(0, 0, 0, 0)
 
-      button_widget = QWidget()
+      button_widget: QWidget = QWidget()
       button_widget.setLayout(button_layout)
 
       self.table_widget.setCellWidget(row_position, 4, button_widget)
