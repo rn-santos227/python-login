6c9cc4213d8bce27f29af0636aebbe6e1d002ea2
rn-santos227 <rn_santos227@yahoo.com>
2024-08-02 19:34:10 +0800

    updated init ui function in scanner view

--- modules/scanner/view.py
+++ modules/scanner/view.py
@@ -29,7 +29,7 @@ class ScannerPage(QWidget):
     webcam_center_layout: QHBoxLayout = QHBoxLayout()
 
     top_spacer: QSpacerItem = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
-    bottom_spacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+    bottom_spacer: QSpacerItem = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
     left_spacer = QSpacerItem(40, 10, QSizePolicy.Expanding, QSizePolicy.Minimum)
     right_spacer = QSpacerItem(40, 10, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
