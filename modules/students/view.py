6ef5b88b20cc80856f1e63b3fc5e3e05852da3ce
rn-santos227 <rn_santos227@yahoo.com>
2024-08-04 20:54:15 +0800

    updated init create layout function in students view

--- modules/students/view.py
+++ modules/students/view.py
@@ -47,7 +47,7 @@ class StudentPage(QWidget):
     self.email_field: TextField = TextField(label_text="Email", placeholder_text="Enter student email.")
     self.password_field: TextField= TextField(label_text="Password", placeholder_text="Enter student password.")
     self.password_field.text_field.setEchoMode(QLineEdit.Password)
-    self.fullname_field = TextField(label_text="Full Name", placeholder_text="Enter student full name.")
+    self.fullname_field: TextField = TextField(label_text="Full Name", placeholder_text="Enter student full name.")
     self.contact_field = TextField(label_text="Contact Number", placeholder_text="Enter student contact number.")
     self.student_number_field = TextField(label_text="Student Number", placeholder_text="Enter student number.")
     self.section_field = TextField(label_text="Student Section", placeholder_text="Enter student section.")
