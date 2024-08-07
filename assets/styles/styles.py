93686dc93380367f4ec67fe5b5782a8a73f81c1c
rn-santos227 <rn_santos227@yahoo.com>
2024-08-07 22:34:08 +0800

    updated auth biew style in styles file

--- assets/styles/styles.py
+++ assets/styles/styles.py
@@ -1,4 +1,6 @@
 auth_view_style = """
-  background-color: rgba(255, 255, 255, 220);
-  border: 2px solid #444;
+  QFrame#formFrame {
+    background-color: rgba(255, 255, 255, 220);
+    border: 2px solid #444;
+  }
 """
