9b66ccf1a03ba69524c68e81edcf9e73f108340e
rn-santos227 <rn_santos227@yahoo.com>
2024-08-08 00:04:22 +0800

    updated auth view style in styles file

--- assets/styles/styles.py
+++ assets/styles/styles.py
@@ -3,4 +3,10 @@ auth_view_style = """
     background-color: rgba(255, 255, 255, 220);
     border: 2px solid #444;
   }
+
+  QLabel#titleLabel {
+    font-size: 20px;
+    font-weight: bold;
+    color: #333; /* Customize the color as needed */
+  }
 """
