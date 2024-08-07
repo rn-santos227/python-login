61bd724aa03a60e2602432b8edf9aab086f54737
rn-santos227 <rn_santos227@yahoo.com>
2024-08-07 22:22:39 +0800

    updated init ui function in auth view

--- modules/auth/view.py
+++ modules/auth/view.py
@@ -40,6 +40,7 @@ class LoginPage(QWidget):
     shadow_effect: QGraphicsDropShadowEffect = QGraphicsDropShadowEffect()
     shadow_effect.setBlurRadius(15)
     shadow_effect.setColor(QColor(0, 0, 0, 160))
+    shadow_effect.setOffset(0, 5)
 
     form_layout: QVBoxLayout = QVBoxLayout(form_frame)
 
