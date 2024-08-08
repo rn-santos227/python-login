f800020820f3da44f36007974d8c98ce52965e93
rn-santos227 <rn_santos227@yahoo.com>
2024-08-08 20:38:49 +0800

    updated init ui function in students view

--- modules/students/view.py
+++ modules/students/view.py
@@ -86,7 +86,7 @@ class StudentPage(QWidget):
     self.update_password_field.text_field.setEchoMode(QLineEdit.Password)
     self.update_fullname_field: TextField = TextField(label_text="Full Name", placeholder_text="Enter student full name.")
     self.update_contact_field: TextField = TextField(label_text="Contact Number", placeholder_text="Enter student contact number.")
-    self.update_student_number_field = TextField(label_text="Student Number", placeholder_text="Enter student number.")
+    self.update_student_number_field: TextField = TextField(label_text="Student Number", placeholder_text="Enter student number.")
     self.update_section_field = TextField(label_text="Student Section", placeholder_text="Enter student section.")
     self.update_grade_field = TextField(label_text="Student Grade", placeholder_text="Enter student grade.")
 
