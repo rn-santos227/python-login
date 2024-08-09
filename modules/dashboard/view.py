0ab3719f2ce1aee4792d3796e50b7d8f2d6f263d
rn-santos227 <rn_santos227@yahoo.com>
2024-08-09 21:15:37 +0800

    updated init ui function in dashboard view

--- modules/dashboard/view.py
+++ modules/dashboard/view.py
@@ -91,6 +91,7 @@ class DashboardAdminPage(QWidget):
     logs_button.set_color(bg_color="cyan", font_color="black")
     reader_button.set_color(bg_color="cyan", font_color="black")
     scanner_button.set_color(bg_color="cyan", font_color="black")
+    students_button.set_color(bg_color="cyan", font_color="black")
 
   def handle_admins(self):
     self.reader_content.clock_component.start_clock()
