9f6664fbe5e6a7369d025624c612df483deed84c
rn-santos227 <rn_santos227@yahoo.com>
2024-08-18 22:00:22 +0800

    updated init function in label component

--- components/label.py
+++ components/label.py
@@ -2,5 +2,5 @@ from PyQt5.QtWidgets import QLabel, QWidget
 from PyQt5.QtGui import QFont
 
 class Label(QWidget):
-  def __init__(self, label_text="Text Field"):
-    pass
+  def __init__(self, label_text="Text Field", parent=None):
+    super().__init__(parent)
