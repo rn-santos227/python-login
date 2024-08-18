129371076fce28a181d5d331754b3b3dd96dad11
rn-santos227 <rn_santos227@yahoo.com>
2024-08-18 22:18:54 +0800

    imported admin dashboard style in dashboard view

--- modules/dashboard/view.py
+++ modules/dashboard/view.py
@@ -13,6 +13,8 @@ from modules.students.view import StudentPage
 
 from handlers.asset_handler import AssetHandler
 
+from assets.styles.styles import admin_dashboard_style
+
 class DashboardAdminPage(QWidget):
   def __init__(self, pages_handler):
     super().__init__()
