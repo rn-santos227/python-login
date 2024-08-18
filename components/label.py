bea0535ed210864ac19982dfc0eb10bf2f08a109
rn-santos227 <rn_santos227@yahoo.com>
2024-08-18 22:00:49 +0800

    added init ui function in label component

--- components/label.py
+++ components/label.py
@@ -4,3 +4,6 @@ from PyQt5.QtGui import QFont
 class Label(QWidget):
   def __init__(self, label_text="Text Field", parent=None):
     super().__init__(parent)
+
+  def init_ui(self, label_text):
+    pass
