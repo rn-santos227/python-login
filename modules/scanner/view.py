c1f529a2697328e4e40f97a84980cf734eddec2d
rn-santos227 <rn_santos227@yahoo.com>
2024-08-20 07:59:47 +0800

    updated init ui function in scanner view

--- modules/scanner/view.py
+++ modules/scanner/view.py
@@ -28,6 +28,7 @@ class ScannerPage(QWidget):
   def init_ui(self):
     content_frame: QFrame = QFrame(self)
     content_frame.setObjectName("contentFrame")
+    content_layout: QVBoxLayout = QVBoxLayout(content_frame)
 
     self.main_layout: QVBoxLayout = QVBoxLayout()
     center_layout: QVBoxLayout = QVBoxLayout()
