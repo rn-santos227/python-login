97174e8bf388e6d280c62a421e21cd86175636e4
rn-santos227 <rn_santos227@yahoo.com>
2024-08-20 07:51:33 +0800

    updated init ui function in logs view

--- modules/logs/view.py
+++ modules/logs/view.py
@@ -25,6 +25,7 @@ class LogsPage(QWidget):
     content_layout: QVBoxLayout = QVBoxLayout(content_frame)
 
     shadow_effect: QGraphicsDropShadowEffect = QGraphicsDropShadowEffect()
+    shadow_effect.setBlurRadius(15)
     
     self.main_layout: QVBoxLayout = QVBoxLayout()
     self.top_layout: QHBoxLayout = QHBoxLayout()
