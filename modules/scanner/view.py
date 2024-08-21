a68c7eae32bb4a04ea12ffb3ef382c21f2f80d94
rn-santos227 <rn_santos227@yahoo.com>
2024-08-21 20:05:42 +0800

    updated init ui function in scanner view

--- modules/scanner/view.py
+++ modules/scanner/view.py
@@ -33,6 +33,7 @@ class ScannerPage(QWidget):
 
     shadow_effect: QGraphicsDropShadowEffect = QGraphicsDropShadowEffect()
     shadow_effect.setBlurRadius(15)
+    shadow_effect.setColor(QColor(0, 0, 0, 160))
 
     self.main_layout: QVBoxLayout = QVBoxLayout()
     center_layout: QVBoxLayout = QVBoxLayout()
