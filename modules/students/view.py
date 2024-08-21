1657901bb345f48277c5d80549da988dbb7a4195
rn-santos227 <rn_santos227@yahoo.com>
2024-08-21 20:08:35 +0800

    updated init ui function in students view

--- modules/students/view.py
+++ modules/students/view.py
@@ -24,7 +24,8 @@ class StudentPage(QWidget):
 
   def init_ui(self):
     content_frame: QFrame = QFrame(self)
-    
+    content_frame.setObjectName("contentFrame")
+
     self.main_layout: QVBoxLayout = QVBoxLayout()
     self.top_layout: QHBoxLayout = QHBoxLayout()
 
