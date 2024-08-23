f60f5cc160bfad960bd27b2b1dd95e87a60ca260
rn-santos227 <rn_santos227@yahoo.com>
2024-08-23 15:46:14 +0800

    updated init ui function in admin view

--- modules/admin/view.py
+++ modules/admin/view.py
@@ -27,7 +27,8 @@ class AdminsPage(QWidget):
 
   def init_ui(self):
     content_frame: QFrame = QFrame(self)
-    
+    content_frame.setObjectName("contentFrame")
+
     self.main_layout: QVBoxLayout = QVBoxLayout()
     self.top_layout: QHBoxLayout = QHBoxLayout()
     
