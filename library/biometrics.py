186f48d6e2270e1f5e6e8498d07b481b06ca8c47
rn-santos227 <rn_santos227@yahoo.com>
2024-08-24 00:27:19 +0800

    added DPFPDD IMAGE in biometrics library

--- library/biometrics.py
+++ library/biometrics.py
@@ -8,3 +8,6 @@ class DPFPDD_DEV_INFO(ctypes.Structure):
 
 class DPFPDD_CAPTURE_RESULT(ctypes.Structure):
   _fields_ = [("quality", ctypes.c_int), ("size", ctypes.c_int)]
+
+class DPFPDD_IMAGE(ctypes.Structure):
+  _fields_ = [("width", ctypes.c_int), ("height", ctypes.c_int), ("image", ctypes.POINTER(ctypes.c_ubyte))]
