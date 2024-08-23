76d666442aa990d87d7041e4f0e31118a7a71dcc
rn-santos227 <rn_santos227@yahoo.com>
2024-08-23 15:27:38 +0800

    updated biometrics handler

--- handlers/biometrics_handler.py
+++ handlers/biometrics_handler.py
@@ -4,4 +4,4 @@ from config.config import fingerjet_url
 
 class BiometricsHandler:
   def __init__(self):
-     self.fingerjet_lib = ctypes.WinDLL(fingerjet_url)
+    self.fingerjet_lib = ctypes.WinDLL(fingerjet_url)
