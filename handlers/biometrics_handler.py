27cff3bb93bbf495955576690f86279d79983905
rn-santos227 <rn_santos227@yahoo.com>
2024-08-23 11:39:38 +0800

    updated init function in biometrics handler

--- handlers/biometrics_handler.py
+++ handlers/biometrics_handler.py
@@ -2,4 +2,9 @@ import win32com.client
 
 class BiometricsHandler:
   def __init__(self):
-    pass
+    try:
+      self.device_control = win32com.client.Dispatch("DPFPDevX.DeviceControl")
+
+    except Exception as e:
+      print(f"Failed to initialize fingerprint scanner: {e}")
+      self.device = None
