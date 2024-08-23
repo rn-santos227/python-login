55c4668ce7bfaa18aad8fcda880af9b4a830527c
rn-santos227 <rn_santos227@yahoo.com>
2024-08-23 14:22:04 +0800

    removed pywin32 in requirements

--- handlers/biometrics_handler.py
+++ handlers/biometrics_handler.py
@@ -1,13 +1,1 @@
-import win32com.client
-
-class BiometricsHandler:
-  def __init__(self):
-    try:
-      self.device_control = win32com.client.Dispatch("DPFPDevX.DeviceControl")
-      self.device = self.device_control.Devices(0)
-      self.device.Open(1)
-      print("Fingerprint scanner initialized successfully.")
-
-    except Exception as e:
-      print(f"Failed to initialize fingerprint scanner: {e}")
-      self.device = None
+import ctypes
