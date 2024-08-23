33c643a66863ccf7e95ae7242565d7c30d1a6afb
rn-santos227 <rn_santos227@yahoo.com>
2024-08-24 00:26:54 +0800

    added DPFPDD DEV INFO in biometrics library

--- library/biometrics.py
+++ library/biometrics.py
@@ -5,3 +5,6 @@ MAX_PATH = 260
 
 class DPFPDD_DEV_INFO(ctypes.Structure):
   _fields_ = [("size", ctypes.c_int), ("name", ctypes.c_char * MAX_PATH)]
+
+class DPFPDD_CAPTURE_RESULT(ctypes.Structure):
+  _fields_ = [("quality", ctypes.c_int), ("size", ctypes.c_int)]
