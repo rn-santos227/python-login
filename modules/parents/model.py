0d83520ef202070cd85973da45cc5a70aa8082c6
rn-santos227 <rn_santos227@yahoo.com>
2024-08-25 21:19:33 +0800

    updated parents model in perents module

--- modules/parents/model.py
+++ modules/parents/model.py
@@ -2,11 +2,11 @@ from modules.students.model import Student
 from modules.students.controller import get_student_by_id
 
 class Parent:
-  def __init__(self, id=None, student_id=None, email=None, full_name=None, contact=None):
+  def __init__(self, id=None, student_id=None,full_name=None, email=None, contact=None):
     self.id = id
     self.student_id = student_id
-    self.email = email
     self.full_name = full_name
+    self.email = email
     self.contact = contact
 
   @property
@@ -20,6 +20,7 @@ class Parent:
       id INT AUTO_INCREMENT PRIMARY KEY,
       student_id INT,
       full_name VARCHAR(255),
+      email VARCHAR(255),
       contact VARCHAR(25),
       CONSTRAINT parent_student FOREIGN KEY (student_id) REFERENCES students(id)
     );
