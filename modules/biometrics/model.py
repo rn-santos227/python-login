e94216e20db1316c9bc6d1d0219627603fa706c4
rn-santos227 <rn_santos227@yahoo.com>
2024-08-25 13:11:39 +0800

    added create table function in biometric model

--- modules/biometrics/model.py
+++ modules/biometrics/model.py
@@ -3,3 +3,9 @@ class Biometric():
     self.id = id
     self.student_id = student_id
     self.fingerprint_data = fingerprint_data
+
+  @staticmethod
+  def create_table() -> str:
+    return '''
+    
+    '''  
