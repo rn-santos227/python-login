0b08118949961235a7dbe61307a4077554a25f63
rn-santos227 <rn_santos227@yahoo.com>
2024-08-25 13:10:53 +0800

    updated biometric model in biometric module

--- modules/biometrics/model.py
+++ modules/biometrics/model.py
@@ -2,3 +2,4 @@ class Biometric():
   def __init__(self, id=None, student_id=None, fingerprint_data=None):
     self.id = id
     self.student_id = student_id
+    self.fingerprint_data = fingerprint_data
