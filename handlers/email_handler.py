1b01497183efbcc5c2358f85ef7de93513a6a687
rn-santos227 <rn_santos227@yahoo.com>
2024-08-26 13:46:07 +0800

    added send email function in email handler

--- handlers/email_handler.py
+++ handlers/email_handler.py
@@ -1,3 +1,8 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+
+from config.config import smtp_server, smtp_port, email_user, email_password
+
+def send_email(receiver_email, subject, message):
+  pass
