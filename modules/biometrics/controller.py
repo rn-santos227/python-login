e0f54a6c061e0e657a77f50f5ef6f97016cb75bc
rn-santos227 <rn_santos227@yahoo.com>
2024-08-27 22:40:57 +0800

    updated get biometrics function in biometrics controller

--- modules/biometrics/controller.py
+++ modules/biometrics/controller.py
@@ -4,6 +4,7 @@ from typing import Union
 from database.query import builder
 from modules.biometrics.model import Biometric
 
+<<<<<<< HEAD
 __table = "biometrics"
 
 def get_biometrics(query, action) -> list[Biometric]:
@@ -34,3 +35,6 @@ def add_biometric():
 
 def delete_biometric():
   pass
+=======
+__table = "biometrics"
+>>>>>>> parent of 63a6158 (added get biometrics function in biometrics controller)
