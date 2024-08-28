a2a1bcc1656c77c7b8bb242f8697adeb6aa070ec
rn-santos227 <rn_santos227@yahoo.com>
2024-08-28 20:28:58 +0800

    updated remove biometric function in biometrics controller

--- modules/biometrics/controller.py
+++ modules/biometrics/controller.py
@@ -39,3 +39,4 @@ def remove_biometric(id) -> bool:
   where_clause = f"id = {id}"
   sql_query = builder(__table, where_clause, "delete")
   connection = DB.connect_db()
+  cursor = connection.cursor()
