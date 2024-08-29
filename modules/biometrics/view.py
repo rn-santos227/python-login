0bdd9519d6a9bfca968500ef8781f0ffef6a9b98
rn-santos227 <rn_santos227@yahoo.com>
2024-08-29 21:36:08 +0800

    imported message box in biometrics view

--- modules/biometrics/view.py
+++ modules/biometrics/view.py
@@ -6,4 +6,5 @@ from components.message_box import MessageBox
 class BiometricsPage(QWidget):
   def __init__(self, pages_handler):
     super().__init__()
+    self.message_box: MessageBox = MessageBox(self)
     self.pages_handler = pages_handler
