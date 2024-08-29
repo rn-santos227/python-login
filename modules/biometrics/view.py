a5c3892e65dcf9b90aa95c3704828cccd172a776
rn-santos227 <rn_santos227@yahoo.com>
2024-08-29 21:35:29 +0800

    imported message box in biometrics view

--- modules/biometrics/view.py
+++ modules/biometrics/view.py
@@ -1,6 +1,8 @@
 from PyQt5.QtGui import QColor
 from PyQt5.QtWidgets import QWidget
 
+from components.message_box import MessageBox
+
 class BiometricsPage(QWidget):
   def __init__(self, pages_handler):
     super().__init__()
