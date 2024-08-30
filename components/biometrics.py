f8f678e0ca7e63fb55675496c8033df4efb80afd
rn-santos227 <rn_santos227@yahoo.com>
2024-08-30 21:41:59 +0800

    updated init function in biometrics component

--- components/biometrics.py
+++ components/biometrics.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import QLabel, QVBoxLayout, QWidget
 class Biometrics(QWidget):
   def __init__(self, parent=None):
     super().__init__(parent)
+    self.init_ui()
 
   def init_ui(self):
     self.layout: QVBoxLayout = QVBoxLayout(self)
