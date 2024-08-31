e690be3158e2d016ac4d8def8ffc1cf3f2cf9957
rn-santos227 <rn_santos227@yahoo.com>
2024-08-31 20:42:08 +0800

    updated init function in biometrics component

--- components/biometrics.py
+++ components/biometrics.py
@@ -5,6 +5,7 @@ from handlers.biometrics_handler import BiometricsHandler
 class Biometrics(QWidget):
   def __init__(self, parent=None):
     super().__init__(parent)
+    self.biometrics_handler: BiometricsHandler = BiometricsHandler()
     self.init_ui()
 
   def init_ui(self):
