bd3b4dd9a75923d5c2c0ea9aa8bd99e7ec0af125
rn-santos227 <rn_santos227@yahoo.com>
2024-08-31 20:41:21 +0800

    imported biometric handler in biometrics component

--- components/biometrics.py
+++ components/biometrics.py
@@ -1,5 +1,7 @@
 from PyQt5.QtWidgets import QLabel, QVBoxLayout, QWidget
 
+from handlers.biometrics_handler import BiometricsHandler
+
 class Biometrics(QWidget):
   def __init__(self, parent=None):
     super().__init__(parent)
