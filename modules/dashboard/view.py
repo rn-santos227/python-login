1595d8938a2aa5cc7c6e2fe638da7fff630d40dd
rn-santos227 <rn_santos227@yahoo.com>
2024-09-02 21:08:33 +0800

    updated init function in dashboard view

--- modules/dashboard/view.py
+++ modules/dashboard/view.py
@@ -19,6 +19,7 @@ from handlers.asset_handler import AssetHandler
 class DashboardAdminPage(QWidget):
   def __init__(self, pages_handler):
     super().__init__()
+    self.biometrics_handler: BiometricsHandler = BiometricsHandler()
     self.pages_handler = pages_handler
     self.navigation_visible = True
     self.init_ui()
