a0701e26a9da34248ebeb284f253c8d84eabc9e0
rn-santos227 <rn_santos227@yahoo.com>
2024-09-03 20:21:42 +0800

    updated biometrics handler

--- handlers/biometrics_handler.py
+++ handlers/biometrics_handler.py
@@ -1,5 +1,4 @@
-import ctypes
-from ctypes import byref
+import comtypes.client
 
 from config.config import dpfj_url, dpfpdd_url
 
@@ -10,51 +9,10 @@ class BiometricsHandler:
     self.initialize()
 
   def initialize(self):
-    self.dpfj = ctypes.WinDLL(dpfj_url)
-    self.dpfpdd = ctypes.WinDLL(dpfpdd_url) 
-    result = self.dpfpdd.dpfpdd_init()
-
-    if result != DPFPDD_SUCCESS:
-      print(f"Error initializing SDK: {result}")
-
-    else:
-      print("SDK initialized successfully.")
+    pass
 
   def capture_fingerprint(self):
-    device = ctypes.c_void_p()
-    result = self.dpfpdd.dpfpdd_open("Device1".encode('utf-8'), byref(device))
-
-    if result != DPFPDD_SUCCESS:
-      print(f"Error opening device: {result}")
-      return None
-    
-    capture_result = DPFPDD_CAPTURE_RESULT()
-    capture_result.size = ctypes.sizeof(capture_result)
-
-    image_buffer = (ctypes.c_ubyte * 100000)()
-    fingerprint_data = bytes(image_buffer)
-
-    if result != DPFPDD_SUCCESS:
-      print(f"Error capturing fingerprint: {result}")
-      return None
-    
-    print(f"Fingerprint captured successfully with quality: {capture_result.quality}")
-    return fingerprint_data
+    pass
 
   def verify_fingerprints(self, fingerprint_1, fingerprint_2) -> bool:
-    features_1 = (ctypes.c_ubyte * 100000)()
-    features_2 = (ctypes.c_ubyte * 100000)()
-
-    self.dpfj.dpfj_create_feature_set(byref(fingerprint_1), byref(features_1))
-    self.dpfj.dpfj_create_feature_set(byref(fingerprint_2), byref(features_2))
-
-    score = ctypes.c_int()
-    result =  self.dpfj.dpfj_verify(byref(features_1), byref(features_2), byref(score))
-
-    if result == DPFPDD_SUCCESS and score.value > 0:
-      print(f"Fingerprints matched with score: {score.value}")
-      return True
-
-    else:
-      print(f"Fingerprints did not match.")
-      return False
+    return False
