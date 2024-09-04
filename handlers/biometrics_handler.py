7ec50db13b46f345489ed8e48a5f687346010cc2
rn-santos227 <rn_santos227@yahoo.com>
2024-09-04 21:37:48 +0800

    updated initialize function in biometrics handler

--- handlers/biometrics_handler.py
+++ handlers/biometrics_handler.py
@@ -14,6 +14,7 @@ class BiometricsHandler:
     self.dpfpdd = ctypes.CDLL("dpfpdd.dll") 
     dpfpdd_init  = self.dpfpdd.dpfpdd_init
     dpfpdd_init.restype = ctypes.c_int
+    result = dpfpdd_init()
 
     if result != DPFPDD_SUCCESS:
       print(f"Error initializing SDK: {result}")
