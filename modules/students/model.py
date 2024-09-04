cd21da8a43f30d1e05fa0d4a1361d37ea047ca78
rn-santos227 <rn_santos227@yahoo.com>
2024-09-04 22:26:29 +0800

    updated create table in students model

--- modules/students/model.py
+++ modules/students/model.py
@@ -22,7 +22,7 @@ class Student(User):
       id INT AUTO_INCREMENT PRIMARY KEY,
       email VARCHAR(255) UNIQUE NOT NULL,
       password VARCHAR(255) NOT NULL,
-      full_name VARCHAR(255),
+      full_name VARCHAR(255) NOT NULL,,
       student_number VARCHAR(255) UNIQUE,
       contact_number VARCHAR(25),
       section VARCHAR(10),
