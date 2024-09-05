ce65f27e82016985523f1ec6a17746f2ec89012e
rn-santos227 <rn_santos227@yahoo.com>
2024-09-05 21:15:05 +0800

    updated init function in faces handler

--- handlers/face_handler.py
+++ handlers/face_handler.py
@@ -2,4 +2,7 @@ import os
 
 class FaceHandler:
   def __init__(self):
-    self.faces_folder = os.path.join(os.environ["USERPROFILE"], "Documents", "Faces")
+    self.documents_folder = os.path.join(os.environ["USERPROFILE"], "Documents", "Faces")
+    
+    if not os.path.exists(self.documents_folder):
+      os.makedirs(self.documents_folder)
