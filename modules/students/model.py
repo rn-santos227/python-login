4b89e75a87e7d82e318db9109e55f879a2680643
rn-santos227 <rn_santos227@yahoo.com>
2024-09-05 19:49:14 +0800

    updated create table function in students model

--- modules/students/model.py
+++ modules/students/model.py
@@ -26,7 +26,7 @@ class Student(User):
       student_number VARCHAR(255) UNIQUE NOT NULL,
       contact_number VARCHAR(25) NOT NULL,
       section VARCHAR(10) NOT NULL,
-      grade VARCHAR(15),
+      grade VARCHAR(15) NOT NULL,
       face_encode TEXT,
       face_url TEXT,
       status VARCHAR(10)
