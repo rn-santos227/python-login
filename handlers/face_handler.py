e4aff6c4124079962c5bb6147b5ba3b06df55dea
rn-santos227 <rn_santos227@yahoo.com>
2024-09-05 21:33:54 +0800

    updated save face function in faces handler

--- handlers/face_handler.py
+++ handlers/face_handler.py
@@ -6,8 +6,8 @@ class FaceHandler:
     self.documents_folder = os.path.join(os.environ["USERPROFILE"], "Documents", "Faces")
     os.makedirs(self.documents_folder, exist_ok=True)
 
-  def save_face(self, image_data, student_id) -> str:
-    image_filename = f"{student_id}_face.png"
+  def save_face(self, image_data, student_number) -> str:
+    image_filename = f"{student_number}_face.png"
     image_path = os.path.join(self.documents_folder, image_filename)
     cv2.imwrite(image_path, image_data)
 
