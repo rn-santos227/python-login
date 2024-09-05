8fb978bb124ecaca3a3659b9c7ee22506e279ac9
rn-santos227 <rn_santos227@yahoo.com>
2024-09-05 21:22:16 +0800

    updated save face function in faces handler

--- handlers/face_handler.py
+++ handlers/face_handler.py
@@ -3,9 +3,7 @@ import os
 class FaceHandler:
   def __init__(self):
     self.documents_folder = os.path.join(os.environ["USERPROFILE"], "Documents", "Faces")
-    
-    if not os.path.exists(self.documents_folder):
-      os.makedirs(self.documents_folder)
+    os.makedirs(self.documents_folder, exist_ok=True)
 
-  def save_face(self, image_data):
-    pass
+  def save_face(self, image_data, student_id):
+    image_filename = f"{student_id}_face.png"
