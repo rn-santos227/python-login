8580ee517048cb429657f46d32959ca8da3e8d6a
rn-santos227 <rn_santos227@yahoo.com>
2024-09-06 21:45:15 +0800

    imported student model in popup dialog component

--- components/popup_dialog.py
+++ components/popup_dialog.py
@@ -1,10 +1,13 @@
 from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtWidgets import QApplication, QFrame, QLabel, QHBoxLayout, QPushButton, QVBoxLayout, QWidget
 
+from modules.students.model import Student
+
 class PopupDialog(QWidget):
   def __init__(self, parent=None):
     super().__init__(parent)
 
-  def init_ui(self):
+  def init_ui(self, student):
     self.setWindowTitle("Information")
     self.layout = QVBoxLayout()
+    self.setLayout(self.layout)
