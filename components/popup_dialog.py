76f80062faaf6924bd154a716cb7d9e7dfe86987
rn-santos227 <rn_santos227@yahoo.com>
2024-09-06 21:58:47 +0800

    updated init uf function in popup dialog component

--- components/popup_dialog.py
+++ components/popup_dialog.py
@@ -2,14 +2,15 @@ from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QFrame, QLabel, QHBoxLayout, QPushButton, QVBoxLayout, QWidget
 
-from modules.logs.model import StudentLog
-
+from modules.students.model import Student
 class PopupDialog(QWidget):
   def __init__(self, parent=None):
     super().__init__(parent)
 
-  def init_ui(self, log: StudentLog):
+  def init_ui(self, student: Student, logged: str):
     self.setWindowTitle("Information")
     self.layout = QVBoxLayout()
     self.setLayout(self.layout)
+
+    self.image_label = QLabel(self)
     
