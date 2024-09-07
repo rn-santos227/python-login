77e053d947884047f8d6979997d50c5c75c838d8
rn-santos227 <rn_santos227@yahoo.com>
2024-09-07 21:26:54 +0800

    updated init ui function in popup dialog components

--- components/popup_dialog.py
+++ components/popup_dialog.py
@@ -35,4 +35,5 @@ class PopupDialog(QWidget):
     self.course_input.setReadOnly(True) 
     self.course_input.setText(student.course)
     self.form_layout.addWidget(self.course_label)
+    self.form_layout.addWidget(self.course_input)
     
