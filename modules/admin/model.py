41702b57b3de89384b68ba6660a1e93419206464
rn-santos227 <rn_santos227@yahoo.com>
2024-09-09 20:15:09 +0800

    updated create table function in admins model

--- modules/admin/model.py
+++ modules/admin/model.py
@@ -17,7 +17,7 @@ class Admin(User):
       id INT AUTO_INCREMENT PRIMARY KEY,
       full_name VARCHAR(255) NOT NULL,
       email VARCHAR(255) UNIQUE,
-      password VARCHAR(255),
+      password VARCHAR(255) NOT NULL,
       status VARCHAR(10)
     );
   '''
