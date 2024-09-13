17b476692b5f96f4758a51238a4eb4d9eeb19c71
rn-santos227 <rn_santos227@yahoo.com>
2024-09-14 00:09:26 +0800

    updated match face function in reader view

--- modules/reader/view.py
+++ modules/reader/view.py
@@ -151,6 +151,7 @@ class ReaderPage(QWidget):
           )
 
           self.popup_dialog.set_student(student=student)
+          self.popup_dialog.set_logged_time(logged=formatted_date_time)
 
           login = logs_controller.create_log(log)
           login.login_time = formatted_date_time
