27d9eeb2dd56e8ebdf6e9c140c3de0d7d7c03cde
rn-santos227 <rn_santos227@yahoo.com>
2024-09-18 21:35:55 +0800

    updated create parent function in parents controller

--- modules/parents/controller.py
+++ modules/parents/controller.py
@@ -70,7 +70,7 @@ def get_parents(query, action) -> list[Parent]:
     cursor.close()
 
 def create_parent(parent: Parent) -> Parent:
-  columns = "(student_id, full_name, contact)" 
+  columns = "(student_id, full_name, email, contact)" 
   sql_query = builder(__table, f"{columns} VALUES (%s, %s, %s)", "insert")
   connection = DB.connect_db()
   cursor = connection.cursor()
