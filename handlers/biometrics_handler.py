56bd6a0041eff4044d396883979f875ada15524a
rn-santos227 <rn_santos227@yahoo.com>
2024-09-19 20:59:02 +0800

    reset verify fingerprint function in biometrics handler

--- handlers/biometrics_handler.py
+++ handlers/biometrics_handler.py
@@ -23,19 +23,4 @@ class BiometricsHandler:
 
 
   def verify_fingerprints(self, fingerprint_1, fingerprint_2) -> bool:
-    features_1 = (ctypes.c_ubyte * 100000)()
-    features_2 = (ctypes.c_ubyte * 100000)()
-
-    self.dpfj.dpfj_create_feature_set(byref(fingerprint_1), byref(features_1))
-    self.dpfj.dpfj_create_feature_set(byref(fingerprint_2), byref(features_2))
-
-    score = ctypes.c_int()
-    result =  self.dpfj.dpfj_verify(byref(features_1), byref(features_2), byref(score))
-
-    if result == DPFPDD_SUCCESS and score.value > 0:
-      print(f"Fingerprints matched with score: {score.value}")
-      return True
-
-    else:
-      print(f"Fingerprints did not match.")
-      return False
+    pass
