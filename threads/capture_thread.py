cd74c580a66dc7beea8ad757f1fbe30f428d41f0
rn-santos227 <rn_santos227@yahoo.com>
2024-09-23 18:53:27 +0800

    added init function in capture thread

--- threads/capture_thread.py
+++ threads/capture_thread.py
@@ -3,3 +3,6 @@ from PyQt5.QtCore import QThread, pyqtSignal
 class CaptureThread(QThread):
   result_ready = pyqtSignal(tuple) 
 
+  def __init__(self, biometrics_handler, device_name):
+    super().__init__()
+
