561b933ea3aeaaa9edac5d2aa87e866a1ec6d872
rn-santos227 <rn_santos227@yahoo.com>
2024-09-24 20:11:15 +0800

    updated init function in capture thread

--- threads/capture_thread.py
+++ threads/capture_thread.py
@@ -3,8 +3,8 @@ from PyQt5.QtCore import QThread, pyqtSignal
 class CaptureThread(QThread):
   result_ready = pyqtSignal(tuple) 
 
-  def __init__(self, biometrics_handler, device_name):
+  def __init__(self, biometrics_handler, device_name: str):
     super().__init__()
     self.biometrics_handler = biometrics_handler
-    self.device_name = device_name
+    self.device_name: str = device_name
 
