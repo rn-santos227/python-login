b7b15e568f7ae1057e664db9233a5006c98a4568
rn-santos227 <rn_santos227@yahoo.com>
2024-09-28 10:02:48 +0800

    udpated init ui function in admin view

--- modules/admin/view.py
+++ modules/admin/view.py
@@ -44,6 +44,8 @@ class AdminsPage(QWidget):
     self.table_widget: QTableWidget = QTableWidget()
     self.table_widget.setColumnCount(4)
     self.table_widget.setHorizontalHeaderLabels(["ID", "Full Name", "Email", "Actions"])
+    self.table_widget.horizontalHeader().setSectionResizeMode(QHeaderView.Fixed)
+    self.table_widget.setColumnWidth(0, 50)
     self.table_widget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
     self.table_widget.verticalHeader().setVisible(False)
 
