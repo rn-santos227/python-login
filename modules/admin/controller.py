22a6acf99bada46f81bda8772e05e5a93af44c02
rn-santos227 <rn_santos227@yahoo.com>
2024-09-30 19:04:32 +0800

    updated get admin by id function in admins controller

--- modules/admin/controller.py
+++ modules/admin/controller.py
@@ -24,9 +24,11 @@ def get_admin_by_id(id) -> Union[Admin, None]:
     
   except Exception as e:
     print(f"Error: {e}")
+    connection.rollback() 
 
   finally:
     cursor.close()
+    connection.close() 
 
 def get_admin_by_email(email: str) -> Union[Admin, None]:
   sql_query = builder(__table, "email = %s", "select")
