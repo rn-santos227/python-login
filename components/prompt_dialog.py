6ffa8d1929dfac5732d844cf1f485b8ec5a4760b
rn-santos227 <rn_santos227@yahoo.com>
2024-10-03 21:20:04 +0800

    added prompt dialog class in prompt dialog component

--- components/prompt_dialog.py
+++ components/prompt_dialog.py
@@ -1,1 +1,5 @@
 from PyQt5.QtWidgets import QDialog, QHBoxLayout, QLabel, QPushButton, QVBoxLayout, QLineEdit
+
+class PromptDialog(QDialog):
+  def __init__(self, title="Input Prompt", message="Please enter something:", parent=None):
+    super().__init__(parent)
