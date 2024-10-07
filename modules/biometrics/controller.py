53956e7fe5793dc578faf91e6bd4742fd67f0fea
rn-santos227 <rn_santos227@yahoo.com>
2024-10-07 21:28:14 +0800

    updated get biometrics bt student id function in biometrics controller

--- modules/biometrics/controller.py
+++ modules/biometrics/controller.py
@@ -7,7 +7,7 @@ from modules.biometrics.model import Biometric, StudentBiometrics
 __table = "biometrics"
 
 def get_biometrics_by_student_id(student_id):
-  pass
+  sql_query = builder(__table, 'student_id = %s', "select")
 
 def get_biometrics(query, action) -> list[Biometric]:
   sql_query = builder(__table, query, action)
