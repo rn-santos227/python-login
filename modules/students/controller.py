7d206e3565fbe0c3347d79053c3fcca19a7e70c1
rn-santos227 <rn_santos227@yahoo.com>
2024-10-07 19:40:54 +0800

    updated get student by id function in student controller

--- modules/students/controller.py
+++ modules/students/controller.py
@@ -24,9 +24,11 @@ def get_student_by_id(id)-> Union[Student, None]:
     
   except Exception as e:
     print(f"Error: {e}")
+    connection.rollback() 
 
   finally:
     cursor.close()
+    connection.close() 
 
 def get_student_by_email(email: str) -> Union[Student, None]:
   sql_query = builder(__table, 'email = %s', "select")
