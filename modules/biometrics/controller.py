39c0754802442dc972874f5657461618117bf656
rn-santos227 <rn_santos227@yahoo.com>
2024-10-08 20:10:19 +0800

    updated get biometrics bt student id function in biometrics controller

--- modules/biometrics/controller.py
+++ modules/biometrics/controller.py
@@ -25,6 +25,7 @@ def get_biometrics_by_student_id(student_id)  -> Union[Biometric, None]:
 
   except Exception as e:
     print(f"Error: {e}")
+    connection.rollback() 
 
 def get_biometrics(query, action) -> list[Biometric]:
   sql_query = builder(__table, query, action)
