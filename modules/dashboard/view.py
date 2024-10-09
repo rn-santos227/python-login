97177219634bd24ea10c9c3f4df754abcfabef69
rn-santos227 <rn_santos227@yahoo.com>
2024-10-09 21:29:42 +0800

    changed logs button color

--- modules/dashboard/view.py
+++ modules/dashboard/view.py
@@ -116,7 +116,7 @@ class DashboardAdminPage(QWidget):
     self.background_label.lower()
     main_layout.setContentsMargins(0, 0, 0, 0)
 
-    logs_button.set_color(bg_color="#fff6f6", font_color="black")
+    logs_button.set_color(bg_color=button_bg_color, font_color=button_font_color)
     scanner_button.set_color(bg_color="#fff6f6", font_color="black")
     students_button.set_color(bg_color="#fff6f6", font_color="black")
     biometrics_button.set_color(bg_color="#fff6f6", font_color="black")
