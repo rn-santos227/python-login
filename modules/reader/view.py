79ef6adaf06e6668b895ff4b3276d81b438c308e
rn-santos227 <rn_santos227@yahoo.com>
2024-10-11 21:19:04 +0800

    update match face function in reader view

--- modules/reader/view.py
+++ modules/reader/view.py
@@ -207,6 +207,7 @@ class ReaderPage(QWidget):
 
         if log is None:
           login = self.create_log(current_date, student.id)
+          login.login_time = formatted_date_time
 
     self.message_dialog.show_message("Information", "No match has been found.", "information")
 
