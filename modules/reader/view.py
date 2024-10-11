1c1fdd65c356106455f67efe5cdb8c8d7cf296a9
rn-santos227 <rn_santos227@yahoo.com>
2024-10-11 21:19:43 +0800

    update match face function in reader view

--- modules/reader/view.py
+++ modules/reader/view.py
@@ -211,7 +211,8 @@ class ReaderPage(QWidget):
 
           logs_controller.add_login_time(login)
           login_message = compose_message(student=student, time=formatted_date_time, logged="logged in")
-
+          send_sms(contact_number=student.contact_number, message=login_message)
+          
     self.message_dialog.show_message("Information", "No match has been found.", "information")
 
   def start_scanner(self):
