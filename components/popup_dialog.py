68d1f1417f6ed21c4467ec7cb2e5e597177ec5b9
rn-santos227 <rn_santos227@yahoo.com>
2024-10-13 00:38:27 +0800

    updated init funciton in popup dialog component

--- components/popup_dialog.py
+++ components/popup_dialog.py
@@ -42,6 +42,7 @@ class PopupDialog(QDialog):
 
     self.student_name_field: TextField = TextField(label_text="Student Name:", placeholder_text="Enter student name.")
     self.student_name_field.set_text("No Student")
+    self.student_name_field.set_text(self.student.full_name)
 
     self.student_course_field: TextField = TextField(label_text="Student Course:", placeholder_text="Enter student course.")
     self.student_course_field.set_text("No Student")
@@ -75,7 +76,7 @@ class PopupDialog(QDialog):
       self.image_label.setText("No Image Available")
 
     if self.student:
-      self.student_name_field.set_text(self.student.full_name)
+
       self.student_course_field.set_text(self.student.course)
       self.student_logged_field.set_text(self.logged)
 
