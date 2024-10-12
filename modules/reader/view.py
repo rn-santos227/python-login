6b6e161dc3dd932951f37dd391becc507e7d18b5
rn-santos227 <rn_santos227@yahoo.com>
2024-10-12 11:37:33 +0800

    update match face function in reader view

--- modules/reader/view.py
+++ modules/reader/view.py
@@ -228,6 +228,7 @@ class ReaderPage(QWidget):
           self.__send_sms_to_parents(student, message=logout_message)
 
         self.popup_dialog.set_student(student=student)
+        self.popup_dialog.set_logged_time(logged=formatted_date_time)
 
     self.message_dialog.show_message("Information", "No match has been found.", "information")
 
