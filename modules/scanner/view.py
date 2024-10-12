b0aff77bc73cc4f871d41c0f592988fd58f91fb2
rn-santos227 <rn_santos227@yahoo.com>
2024-10-12 17:33:31 +0800

    updated save face function in scanner view

--- modules/scanner/view.py
+++ modules/scanner/view.py
@@ -109,7 +109,7 @@ class ScannerPage(QWidget):
     student = students_controller.get_student_by_id(student_id)
     image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    face_locations = face_recognition.face_locations(image_rgb, model="cnn")
+    face_locations = face_recognition.face_locations(image_rgb, model="hog")
 
     if not face_locations:
       self.message_dialog.show_message("Error", "No face detected in the frame. Please try again.", "error")
