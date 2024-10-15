a2638a7c3c9c36b9588e77c0e3c57076558a6185
rn-santos227 <rn_santos227@yahoo.com>
2024-10-15 20:18:32 +0800

    updated search logs function in logs view

--- modules/logs/view.py
+++ modules/logs/view.py
@@ -130,6 +130,8 @@ class LogsPage(QWidget):
       button_widget: QWidget = QWidget()
       button_widget.setLayout(button_layout)
 
+      self.table_widget.setCellWidget(row_position, 4, button_widget)
+
   def delete_log(self, log_id):
     logs_controller.delete_log(id=log_id)
     self.load_logs()
