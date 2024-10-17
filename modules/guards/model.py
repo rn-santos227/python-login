949079676923054a1ececf3738449c91988ab43e
rn-santos227 <rn_santos227@yahoo.com>
2024-10-17 16:52:40 +0800

    updated guard class in guards model

--- modules/guards/model.py
+++ modules/guards/model.py
@@ -4,3 +4,4 @@ class Guard(User):
   def __init__(self, id=None, full_name=None, email=None, password=None, status=None):
     super().__init__(id, email, password)
     self.full_name = full_name
+    self.status = status
