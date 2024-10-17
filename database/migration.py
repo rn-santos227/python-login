871ecb40e94746110e51993a2d832dc2bb77b29a
rn-santos227 <rn_santos227@yahoo.com>
2024-10-17 16:58:18 +0800

    updated table migration function in database migration

--- database/migration.py
+++ database/migration.py
@@ -1,6 +1,7 @@
 import config.database as db
 
 from modules.admin.model import Admin
+from modules.guards.model import Guard
 from modules.students.model import Student
 from modules.logs.model import Log
 from modules.parents.model import Parent
@@ -8,6 +9,7 @@ from modules.biometrics.model import Biometric
 
 def table_migration():
   db.create_table(Admin.create_table(), "admins")
+  db.create_table(Guard.create_table(), "guards")
   db.create_table(Student.create_table(), "students")
   db.create_table(Log.create_table(), "logs")
   db.create_table(Parent.create_table(), "parents")
