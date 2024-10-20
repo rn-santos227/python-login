0a2cc6ec516582d8f7e4c294ba27c308d587e7d3
rn-santos227 <rn_santos227@yahoo.com>
2024-10-20 16:28:36 +0800

    imported packages and libraries in guards view

---
+++ modules/guards/view.py
@@ -0,0 +1,15 @@
+import modules.guards.controller as guards_controller
+
+from PyQt5.QtGui import QColor
+from PyQt5.QtWidgets import QDialog, QFrame, QGraphicsDropShadowEffect, QGridLayout, QHeaderView, QHBoxLayout, QLayout, QLayoutItem, QLineEdit, QPushButton, QSizePolicy, QSpacerItem, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget
+
+from components.button import Button
+from components.message_dialog import MessageDialog
+from components.prompt_dialog import PromptDialog
+from components.text_field import TextField
+
+from handlers.validations_handler import ValidationHandler
+
+from modules.guards.model import Guard
+
+from assets.styles.styles import content_frame_style
