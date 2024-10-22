bc9f5d45feafc1a5854e8dbc9715ebff2928bc75
rn-santos227 <rn_santos227@yahoo.com>
2024-10-23 00:55:55 +0800

    updated create guard function in guards view

--- modules/guards/view.py
+++ modules/guards/view.py
@@ -127,6 +127,7 @@ class GuardsPage(QWidget):
 
     guards_controller.create_guard(new_guard)
     self.load_guards()
+    self.__clear_fields()
 
   def delete_guard(self, guard_id):
     guards_controller.delete_guard(id=guard_id)
