5ea462c26f4a732c3ad6b98e2098833e15a6c92d
rn-santos227 <rn_santos227@yahoo.com>
2024-10-24 20:23:06 +0800

    imported asset handler in dashboard guards page

--- modules/dashboard/guards/views.py
+++ modules/dashboard/guards/views.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QDialog, QFrame, QHBoxLayout, QLabel, QStackedWidget, QVBoxLayout, QWidget
 from PyQt5.QtCore import Qt
 
+from handlers.asset_handler import AssetHandler
 from handlers.session_handler import SessionHandler
 
 class DashboardGuardPage(QWidget):
