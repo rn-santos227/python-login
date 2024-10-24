bd54cfdf964bace7d2a3d7bc6fac828fe6753022
rn-santos227 <rn_santos227@yahoo.com>
2024-10-24 20:20:58 +0800

    updated init function in dashboard guards page

--- modules/dashboard/guards/views.py
+++ modules/dashboard/guards/views.py
@@ -1,7 +1,10 @@
 from PyQt5.QtWidgets import QDialog, QFrame, QHBoxLayout, QLabel, QStackedWidget, QVBoxLayout, QWidget
 from PyQt5.QtCore import Qt
 
+from handlers.session_handler import SessionHandler
+
 class DashboardGuardPage(QWidget):
-  def __init__(self, pages_handler):
+   def __init__(self, pages_handler, session_handler: SessionHandler):
     super().__init__()
     self.pages_handler = pages_handler
+    self.session_handler: SessionHandler = session_handler
