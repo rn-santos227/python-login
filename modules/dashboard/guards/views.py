cc32374ca6fa11c58c2d3fa252dc97d489f1f757
rn-santos227 <rn_santos227@yahoo.com>
2024-10-24 20:12:31 +0800

    added dashboard guard page class in guards dashboard

--- modules/dashboard/guards/views.py
+++ modules/dashboard/guards/views.py
@@ -1,2 +1,7 @@
 from PyQt5.QtWidgets import QDialog, QFrame, QHBoxLayout, QLabel, QStackedWidget, QVBoxLayout, QWidget
 from PyQt5.QtCore import Qt
+
+class DashboardGuardPage(QWidget):
+  def __init__(self, pages_handler):
+    super().__init__()
+    self.pages_handler = pages_handler
