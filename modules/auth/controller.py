17f1a0d976ca32716cf6d43444a0e5bbc16d7b89
rn-santos227 <rn_santos227@yahoo.com>
2024-10-27 21:18:58 +0800

    updated login funciton in auth controller

--- modules/auth/controller.py
+++ modules/auth/controller.py
@@ -4,17 +4,17 @@ import modules.students.controller as student_controller
 from modules.admin.model import Admin
 from modules.students.model import Student
 
-def login(email, password) -> bool:
+def login(email, password) -> str:
   student = student_controller.get_student_by_email(email)
   admin = admin_controller.get_admin_by_email(email)
 
   student_valid = isinstance(student, Student) and student.verify_password(password)
   admin_valid = isinstance(admin, Admin) and admin.verify_password(password)
   
-  if student_valid or admin_valid:
-    return True
+  if admin_valid:
+    return "admin"
   else:
-    return False
+    return ""
   
 def get_user(email):
   student = student_controller.get_student_by_email(email)
