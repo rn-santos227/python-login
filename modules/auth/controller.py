22ba40f08c9fd34dc1b990fc9b07018e681c45c0
rn-santos227 <rn_santos227@yahoo.com>
2024-10-27 21:21:22 +0800

    imported guard model in auth controller

--- modules/auth/controller.py
+++ modules/auth/controller.py
@@ -2,18 +2,20 @@ import modules.admin.controller as admin_controller
 import modules.students.controller as student_controller
 
 from modules.admin.model import Admin
+from modules.guards.model import Guard
 from modules.students.model import Student
 
 def login(email, password) -> str:
   student = student_controller.get_student_by_email(email)
   admin = admin_controller.get_admin_by_email(email)
 
-  student_valid = isinstance(student, Student) and student.verify_password(password)
+  
   admin_valid = isinstance(admin, Admin) and admin.verify_password(password)
+  student_valid = isinstance(student, Student) and student.verify_password(password)
   
   if admin_valid:
     return "admin"
-  
+
   elif student_valid:
     return "student"
 
