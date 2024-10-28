a4c6f8d6a38958e1e58c6c7b1b6eba78f2a507d9
rn-santos227 <rn_santos227@yahoo.com>
2024-10-28 14:53:10 +0800

    updated handle login function in auth view

--- modules/auth/view.py
+++ modules/auth/view.py
@@ -138,7 +138,9 @@ class LoginPage(QWidget):
       self.password_field.clear_text()
       self.email_field.clear_text()
       self.message_dialog.show_message(title="Information", message="Login Successful.")
-      self.pages_handler.switch_to_dashboard_admin_page()
+      
+      if user is "admin":
+        self.pages_handler.switch_to_dashboard_admin_page()
 
     else:
       print("Login Unsuccessful.")
