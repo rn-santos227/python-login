93a4af801fa87309138bca5ebaa22aa0aee6c67f
rn-santos227 <rn_santos227@yahoo.com>
2024-10-30 20:47:01 +0800

    updated create admin function in admins view

--- modules/admin/view.py
+++ modules/admin/view.py
@@ -116,6 +116,7 @@ class AdminsPage(QWidget):
       (self.validation_handler.is_valid_email, email, "Invalid email address."),
       (self.validation_handler.is_unique_admin_email, email, "Email has been taken."),
       (self.validation_handler.is_unique_student_email, email, "Email has been taken."),
+      (self.validation_handler.is_unique_guard_email, email, "Email has been taken."),
       (self.validation_handler.is_not_empty, password, "Password cannot be empty."),
       (self.validation_handler.is_not_empty, full_name, "Full name cannot be empty."),
     ]
