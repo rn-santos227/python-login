0602fd7f1ac6390413c54d831672b27d94dc994f
rn-santos227 <rn_santos227@yahoo.com>
2024-10-31 20:48:16 +0800

    added dashboard student page in dashboard students view

--- modules/dashboard/students/view.py
+++ modules/dashboard/students/view.py
@@ -3,3 +3,7 @@ from PyQt5.QtCore import Qt
 
 from handlers.asset_handler import AssetHandler
 from handlers.session_handler import SessionHandler
+
+class DashboardStudentPage(QWidget):
+  def __init__(self, pages_handler, session_handler: SessionHandler):
+    super().__init__()
