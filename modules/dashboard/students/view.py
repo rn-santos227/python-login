566ee183e9ce2d302dc4e99ad462b3f64437db3d
rn-santos227 <rn_santos227@yahoo.com>
2024-10-31 20:46:55 +0800

    imported handlers in dashboard students view

--- modules/dashboard/students/view.py
+++ modules/dashboard/students/view.py
@@ -1,1 +1,5 @@
 from PyQt5.QtWidgets import QDialog, QFrame, QHBoxLayout, QLabel, QStackedWidget, QVBoxLayout, QWidget
+from PyQt5.QtCore import Qt
+
+from handlers.asset_handler import AssetHandler
+from handlers.session_handler import SessionHandler
