e36851c60c2a07e0508b220cbb923418dfbb681e
rn-santos227 <rn_santos227@yahoo.com>
2024-11-01 20:02:37 +0800

    imported logs page in dashboard students view

--- modules/dashboard/students/view.py
+++ modules/dashboard/students/view.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import QDialog, QFrame, QHBoxLayout, QLabel, QStackedWidget, QVBoxLayout, QWidget
 from PyQt5.QtCore import Qt
 
+from modules.logs.view import LogsPage
+
 from handlers.asset_handler import AssetHandler
 from handlers.session_handler import SessionHandler
 
